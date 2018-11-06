ac7563033b654c9ee98869307e23dbde75aeaef5
Phil Elwell <phil@raspberrypi.org>
2018-11-06 12:57:48 +0000

    sc16is7xx: Don't spin if no data received
    See: https://github.com/raspberrypi/linux/issues/2676
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    (cherry picked from commit e2a827ce89fe3e7ee43ba89deadb3ef1360143a0
    https://github.com/raspberrypi/linux.git rpi-5.3.y)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/tty/serial/sc16is7xx.c
+++ drivers/tty/serial/sc16is7xx.c
@@ -709,6 +709,8 @@ static bool sc16is7xx_port_irq(struct sc16is7xx_port *s, int portno)
 
 			if (rxlen)
 				sc16is7xx_handle_rx(port, rxlen, iir);
+			else
+				return false;
 			break;
 		case SC16IS7XX_IIR_THRI_SRC:
 			sc16is7xx_handle_tx(port);
