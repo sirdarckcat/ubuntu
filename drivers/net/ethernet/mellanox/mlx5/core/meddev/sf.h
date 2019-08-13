99a98e096cfecbf62d4e19af8f3f484697b54a8e
Parav Pandit <parav@mellanox.com>
2019-08-13 10:45:03 +0300

    UBUNTU: SAUCE: net/mlx5: Introduce SF table framework
    Introduce a SF table for SF life cycle for a device which supports SF
    capability.
    This SF table framework is used in subsequent patches.
    
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Vu Pham <vuhuong@mellanox.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.h
+++ drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.h
@@ -7,11 +7,26 @@
 #include <linux/mlx5/driver.h>
 #include <linux/mlx5/eswitch.h>
 
+struct mlx5_sf_table {
+	phys_addr_t base_address;
+	/* Protects sfs life cycle and sf enable/disable flows */
+	struct mutex lock;
+	u16 max_sfs;
+	u16 log_sf_bar_size;
+};
+
 static inline bool mlx5_core_is_sf_supported(const struct mlx5_core_dev *dev)
 {
 	return MLX5_ESWITCH_MANAGER(dev) &&
 	       MLX5_CAP_GEN(dev, max_num_sf_partitions) &&
 	       MLX5_CAP_GEN(dev, sf);
 }
+
+#ifdef CONFIG_MLX5_MDEV
+int mlx5_sf_table_init(struct mlx5_core_dev *dev,
+		       struct mlx5_sf_table *sf_table);
+void mlx5_sf_table_cleanup(struct mlx5_core_dev *dev,
+			   struct mlx5_sf_table *sf_table);
+#endif
 
 #endif
