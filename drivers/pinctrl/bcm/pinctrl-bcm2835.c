1c8c81920df9a9238c1d119a3cd10de88624e301
Phil Elwell <phil@raspberrypi.org>
2019-10-09 17:22:07 +0100

    bcm2711: Retain support for old dtbs
    BugLink: https://bugs.launchpad.net/bugs/1860343
    
    The recent series switching to bcm2711 as the DT identifier broke Pis
    running with old DTBs. Add some bcm2838 compatible strings as a
    temporary measure, at least until the next full Raspbian image with
    bcm2711 DTBs.
    
    See: https://github.com/raspberrypi/linux/pull/3244
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    
    (cherry picked from commit 8092dbb163ad0bc1c1608ea9ca5351e38f968079 rpi-5.3.y)
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/pinctrl/bcm/pinctrl-bcm2835.c
+++ drivers/pinctrl/bcm/pinctrl-bcm2835.c
@@ -1175,6 +1175,11 @@ static const struct of_device_id bcm2835_pinctrl_match[] = {
 		.compatible = "brcm,bcm7211-gpio",
 		.data = &bcm2711_plat_data,
 	},
+	// Temporary, for backwards-compatibility with old DTBs
+	{
+		.compatible = "brcm,bcm2838-gpio",
+		.data = &bcm2711_pinconf_ops,
+	},
 	{}
 };
 
