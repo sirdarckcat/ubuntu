8bf4fc33034fcbb74e17ed284a522cdcff279826
Pablo Neira Ayuso <pablo@netfilter.org>
2019-10-16 14:24:01 +0200

    UBUNTU: SAUCE: netfilter: nf_tables: dynamically allocate hooks per net_device in flowtables
    Use a list of hooks per device instead an array.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (backported from commit 3f0465a9ef02624e0a36db9e7c9bedcafcd6f6fe)
    
    Conflicts:
    	net/netfilter/nf_tables_api.c
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -936,6 +936,12 @@ struct nft_stats {
 	struct u64_stats_sync	syncp;
 };
 
+struct nft_hook {
+	struct list_head	list;
+	struct nf_hook_ops	ops;
+	struct rcu_head		rcu;
+};
+
 /**
  *	struct nft_base_chain - nf_tables base chain
  *
@@ -1169,7 +1175,7 @@ struct nft_flowtable {
 	u32				use;
 	u64				handle;
 	/* runtime data below here */
-	struct nf_hook_ops		*ops ____cacheline_aligned;
+	struct list_head		hook_list ____cacheline_aligned;
 	struct nf_flowtable		data;
 };
 
