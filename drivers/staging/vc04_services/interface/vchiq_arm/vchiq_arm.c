3e6193d26b58dd66180e455c47e3c0470df6bc32
Dave Stevenson <dave.stevenson@raspberrypi.com>
2019-11-06 13:58:18 +0000

    staging: vchiq_arm: Unify the unload handling of platform devs
    BugLink: https://bugs.launchpad.net/bugs/1860343
    
    A helper function vchiq_register_child was added to deal with
    adding the platform devices. This returns NULL on failure, and
    that is assigned to the struct platform_device. There is
    therefore no way for remove to encounter an error pointer, so
    checking for IS_ERR() is redundant.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    
    (cherry picked from commit b371b4e5e64f1f9d919fd2de435bbe6e2c0d447c rpi-5.4.y)
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
@@ -3269,8 +3269,7 @@ failed_platform_init:
 static int vchiq_remove(struct platform_device *pdev)
 {
 	platform_device_unregister(bcm2835_audio);
-	if (!IS_ERR(bcm2835_camera))
-		platform_device_unregister(bcm2835_camera);
+	platform_device_unregister(bcm2835_camera);
 	platform_device_unregister(bcm2835_codec);
 	platform_device_unregister(vcsm_cma);
 	vchiq_debugfs_deinit();
