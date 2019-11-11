19a09318c6d5b2219f6668a8339ad9349f04cd1a
Paul Blakey <paulb@mellanox.com>
2019-11-12 00:34:26 +0100

    net/mlx5: Define fdb tc levels per prio
    Define FDB_TC_LEVELS_PER_PRIO instead of magic number 2.
    This is the number of levels used by each tc prio table in the fdb.
    
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Mark Bloch <markb@mellanox.com>
    Acked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    (cherry-picked from commit 4db7b98e943225dc2a7435811767e44f63640462)
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -48,6 +48,7 @@
 #define FDB_TC_MAX_CHAIN 3
 #define FDB_TC_SLOW_PATH_CHAIN (FDB_TC_MAX_CHAIN + 1)
 #define FDB_TC_MAX_PRIO 16
+#define FDB_TC_LEVELS_PER_PRIO 2
 
 #ifdef CONFIG_MLX5_ESWITCH
 
@@ -155,7 +156,6 @@ enum offloads_fdb_flags {
 
 extern const unsigned int ESW_POOLS[4];
 
-#define PRIO_LEVELS 2
 struct mlx5_eswitch_fdb {
 	union {
 		struct legacy_fdb {
@@ -182,7 +182,7 @@ struct mlx5_eswitch_fdb {
 			struct {
 				struct mlx5_flow_table *fdb;
 				u32 num_rules;
-			} fdb_prio[FDB_TC_MAX_CHAIN + 1][FDB_TC_MAX_PRIO + 1][PRIO_LEVELS];
+			} fdb_prio[FDB_TC_MAX_CHAIN + 1][FDB_TC_MAX_PRIO + 1][FDB_TC_LEVELS_PER_PRIO];
 			/* Protects fdb_prio table */
 			struct mutex fdb_prio_lock;
 
