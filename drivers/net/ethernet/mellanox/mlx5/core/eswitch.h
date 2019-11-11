0d2dd4b0bd26b10ee00969c2381ceb7ca1d9167e
Paul Blakey <paulb@mellanox.com>
2019-11-12 00:34:25 +0100

    UBUNTU: SAUCE: net/mlx5: Rename FDB_* tc related defines to FDB_TC_* defines
    Rename it to prepare for next patch that will add a
    different type of offload to the FDB.
    
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Mark Bloch <markb@mellanox.com>
    Acked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -45,9 +45,9 @@
 #include "mlx5_core.h"
 #include "meddev/sf.h"
 
-#define FDB_MAX_CHAIN 3
-#define FDB_SLOW_PATH_CHAIN (FDB_MAX_CHAIN + 1)
-#define FDB_MAX_PRIO 16
+#define FDB_TC_MAX_CHAIN 3
+#define FDB_TC_SLOW_PATH_CHAIN (FDB_TC_MAX_CHAIN + 1)
+#define FDB_TC_MAX_PRIO 16
 
 #ifdef CONFIG_MLX5_ESWITCH
 
@@ -182,7 +182,7 @@ struct mlx5_eswitch_fdb {
 			struct {
 				struct mlx5_flow_table *fdb;
 				u32 num_rules;
-			} fdb_prio[FDB_MAX_CHAIN + 1][FDB_MAX_PRIO + 1][PRIO_LEVELS];
+			} fdb_prio[FDB_TC_MAX_CHAIN + 1][FDB_TC_MAX_PRIO + 1][PRIO_LEVELS];
 			/* Protects fdb_prio table */
 			struct mutex fdb_prio_lock;
 
