6703a67b00653965c26fba9239e5f247aa78a6e4
Raed Salem <raeds@mellanox.com>
2019-11-18 14:30:20 +0200

    UBUNTU: SAUCE: net/mlx5: Accel, Add core IPsec support for the Connect-X family
    This to set the base for downstream patches to support
    the new IPsec implementation of the Connect-X family.
    
    Following modifications made:
    - Remove accel layer dependency from MLX5_FPGA_IPSEC.
    - Introduce accel_ipsec_ops, each IPsec device will
      have to support these ops.
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1119,11 +1119,7 @@ static int mlx5_load(struct mlx5_core_dev *dev)
 		goto err_fpga_start;
 	}
 
-	err = mlx5_accel_ipsec_init(dev);
-	if (err) {
-		mlx5_core_err(dev, "IPSec device start failed %d\n", err);
-		goto err_ipsec_start;
-	}
+	mlx5_accel_ipsec_init(dev);
 
 	err = mlx5_accel_tls_init(dev);
 	if (err) {
@@ -1165,7 +1161,6 @@ err_fs:
 	mlx5_accel_tls_cleanup(dev);
 err_tls_start:
 	mlx5_accel_ipsec_cleanup(dev);
-err_ipsec_start:
 	mlx5_fpga_device_stop(dev);
 err_fpga_start:
 	mlx5_hv_vhca_cleanup(dev->hv_vhca);
@@ -1650,7 +1645,7 @@ static int __init init(void)
 	get_random_bytes(&sw_owner_id, sizeof(sw_owner_id));
 
 	mlx5_core_verify_params();
-	mlx5_accel_ipsec_build_fs_cmds();
+	mlx5_fpga_ipsec_build_fs_cmds();
 	mlx5_register_debugfs();
 
 	err = pci_register_driver(&mlx5_core_driver);
