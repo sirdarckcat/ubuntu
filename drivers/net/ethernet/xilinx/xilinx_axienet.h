adb4c54bb84528f19c5c3b0fcc2d4e433f7d2532
Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
2020-01-24 20:52:25 +0530

    net: axienet: Add support for 2.5G MAC
    This patch adds support for 2.5G MAC in the driver.
    
    Signed-off-by: Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>
    
    Conflicts:
    	drivers/net/ethernet/xilinx/xilinx_axienet_main.c

--- drivers/net/ethernet/xilinx/xilinx_axienet.h
+++ drivers/net/ethernet/xilinx/xilinx_axienet.h
@@ -275,6 +275,7 @@
 #define XAE_EMMC_LINKSPD_10	0x00000000 /* Link Speed mask for 10 Mbit */
 #define XAE_EMMC_LINKSPD_100	0x40000000 /* Link Speed mask for 100 Mbit */
 #define XAE_EMMC_LINKSPD_1000	0x80000000 /* Link Speed mask for 1000 Mbit */
+#define XAE_EMMC_LINKSPD_2500	0x80000000 /* Link Speed mask for 2500 Mbit */
 
 /* Bit masks for Axi Ethernet PHYC register */
 #define XAE_PHYC_SGMIILINKSPEED_MASK	0xC0000000 /* SGMII link speed mask*/
@@ -345,6 +346,24 @@
 #define DELAY_OF_ONE_MILLISEC		1000
 
 #define XAXIENET_NAPI_WEIGHT		64
+
+/* XXV MAC Register Definitions */
+#define XXV_TC_OFFSET			0x0000000C
+#define XXV_RCW1_OFFSET			0x00000014
+#define XXV_JUM_OFFSET			0x00000018
+#define XXV_TICKREG_OFFSET		0x00000020
+#define XXV_STATRX_BLKLCK_OFFSET	0x0000040C
+
+/* XXV MAC Register Mask Definitions */
+#define XXV_TC_TX_MASK		BIT(0)
+#define XXV_RCW1_RX_MASK	BIT(0)
+#define XXV_RCW1_FCS_MASK	BIT(1)
+#define XXV_TC_FCS_MASK		BIT(1)
+#define XXV_MIN_JUM_MASK	GENMASK(7, 0)
+#define XXV_MAX_JUM_MASK	GENMASK(10, 8)
+#define XXV_RX_BLKLCK_MASK	BIT(0)
+#define XXV_TICKREG_STATEN_MASK BIT(0)
+#define XXV_MAC_MIN_PKT_LEN	64
 
 /**
  * struct axidma_bd - Axi Dma buffer descriptor layout
@@ -480,10 +499,16 @@ struct axienet_local {
  * enum axienet_ip_type - AXIENET IP/MAC type.
  *
  * @XAXIENET_1G:	 IP is 1G MAC
+ * @XAXIENET_2_5G:	 IP type is 2.5G MAC.
+ * @XAXIENET_LEGACY_10G: IP type is legacy 10G MAC.
+ * @XAXIENET_10G_25G:	 IP type is 10G/25G MAC(XXV MAC).
  *
  */
 enum axienet_ip_type {
 	XAXIENET_1G = 0,
+	XAXIENET_2_5G,
+	XAXIENET_LEGACY_10G,
+	XAXIENET_10G_25G,
 };
 
 struct axienet_config {
@@ -498,6 +523,12 @@ struct axienet_config {
  * @m_or:	Mask to be ORed for setting the option in the register
  */
 struct axienet_option {
+	u32 opt;
+	u32 reg;
+	u32 m_or;
+};
+
+struct xxvenet_option {
 	u32 opt;
 	u32 reg;
 	u32 m_or;
@@ -542,5 +573,6 @@ int axienet_mdio_enable(struct axienet_local *lp);
 void axienet_mdio_disable(struct axienet_local *lp);
 int axienet_mdio_setup(struct axienet_local *lp);
 void axienet_mdio_teardown(struct axienet_local *lp);
+int axienet_mdio_wait_until_ready(struct axienet_local *lp);
 
 #endif /* XILINX_AXI_ENET_H */
