52b2e8e90a2e20ba96372a90b63b200b8326865b
Paul Blakey <paulb@mellanox.com>
2020-03-17 16:32:21 +0200

    UBUNTU: SAUCE: net/mlx5e: Export sharing of mod headers to a new file
    Refactor sharing of mod headers to new file and while there,
    remove spin lock and flows list, as this is only used for warn on.
    
    Use the generic API in the next patch to re-use tuple modify headers
    for identical modify actions,
    
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

---
+++ drivers/net/ethernet/mellanox/mlx5/core/en/mod_hdr.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
+/* Copyright (c) 2020 Mellanox Technologies */
+
+#ifndef __MLX5E_EN_MOD_HDR_H__
+#define __MLX5E_EN_MOD_HDR_H__
+
+#include <linux/hashtable.h>
+#include <linux/mlx5/fs.h>
+
+#include "../fs_core.h"
+#include "../en.h"
+#include "../en_tc.h"
+
+struct mlx5e_mod_hdr_handle;
+
+struct mlx5e_mod_hdr_handle *
+mlx5e_mod_hdr_create(struct mlx5e_priv *priv,
+		     struct mod_hdr_tbl *tbl,
+		     enum mlx5_flow_namespace_type namespace,
+		     struct mlx5e_tc_mod_hdr_acts *mod_hdr_acts);
+void mlx5e_mod_hdr_destroy(struct mlx5e_priv *priv, struct mod_hdr_tbl *tbl,
+			   struct mlx5e_mod_hdr_handle *mh);
+struct mlx5_modify_hdr *mlx5e_mod_hdr_get(struct mlx5e_mod_hdr_handle *mh);
+
+void mlx5e_mod_hdr_tbl_init(struct mod_hdr_tbl *tbl);
+void mlx5e_mod_hdr_tbl_destroy(struct mod_hdr_tbl *tbl);
+
+#endif /* __MLX5E_EN_MOD_HDR_H__ */
