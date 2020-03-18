d433c4571c1eb1dd394ebf068a8c8dbebbf4bee1
Daniel Borkmann <daniel@iogearbox.net>
2020-03-18 10:33:22 +0100

    netfilter: revert introduction of egress hook
    This reverts the following commits:
    
      8537f78647c0 ("netfilter: Introduce egress hook")
      5418d3881e1f ("netfilter: Generalize ingress hook")
      b030f194aed2 ("netfilter: Rename ingress hook include file")
    
    >From the discussion in [0], the author's main motivation to add a hook
    in fast path is for an out of tree kernel module, which is a red flag
    to begin with. Other mentioned potential use cases like NAT{64,46}
    is on future extensions w/o concrete code in the tree yet. Revert as
    suggested [1] given the weak justification to add more hooks to critical
    fast-path.
    
      [0] https://lore.kernel.org/netdev/cover.1583927267.git.lukas@wunner.de/
      [1] https://lore.kernel.org/netdev/20200318.011152.72770718915606186.davem@davemloft.net/
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Cc: David Miller <davem@davemloft.net>
    Cc: Pablo Neira Ayuso <pablo@netfilter.org>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Nacked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry-picked from commit 357b6cc5834eabc1be7c28a9faae7da061df097d)
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1727,7 +1727,6 @@ enum netdev_ml_priv_type {
  *	@xps_maps:	XXX: need comments on this one
  *	@miniq_egress:		clsact qdisc specific data for
  *				egress processing
- *	@nf_hooks_egress:	netfilter hooks executed for egress packets
  *	@qdisc_hash:		qdisc hash table
  *	@watchdog_timeo:	Represents the timeout that is used by
  *				the watchdog (see dev_watchdog())
@@ -2001,9 +2000,6 @@ struct net_device {
 #endif
 #ifdef CONFIG_NET_CLS_ACT
 	struct mini_Qdisc __rcu	*miniq_egress;
-#endif
-#ifdef CONFIG_NETFILTER_EGRESS
-	struct nf_hook_entries __rcu *nf_hooks_egress;
 #endif
 
 	/* These may be needed for future network-power-down code. */
