huynguyen85/temp_ipsec_inline_ofed_4_7

commit df7a26dbb4ffd3ad4805cd0bb588d1b8029d74f7

net/mlx5: Introduce SF life cycle APIs to allocate/free
Introduce SF life cycle APIs to allocate, deallocate it at device
level.
Make use of low level device life cycle APIs and provide higher level
API for a usable SF creation/deletion.

Issue: 1334821
Change-Id: Iee90459e99c1ef912a61e353635e5e6b3a46aee4
Signed-off-by: Parav Pandit <parav@mellanox.com>
Signed-off-by: Vu Pham <vuhuong@mellanox.com>

drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.c
@@ -3,9 +3,13 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/io-mapping.h>
+#include <linux/mlx5/driver.h>
+#include <linux/bitmap.h>
 #include "eswitch.h"
 #include "sf.h"
 #include "mlx5_core.h"
+#include "eswitch.h"
 
 static int
 mlx5_cmd_query_sf_partitions(struct mlx5_core_dev *mdev, u32 *out, int outlen)
@@ -51,6 +55,12 @@ int mlx5_sf_table_init(struct mlx5_core_dev *dev,
 	mlx5_core_dbg(dev, "SF_part(0) log_num_sf(%d) log_sf_bar_size(%d)\n",
 		      sf_table->max_sfs, sf_table->log_sf_bar_size);
 
+	sf_table->sf_id_bitmap = bitmap_zalloc(sf_table->max_sfs, GFP_KERNEL);
+	if (!sf_table->sf_id_bitmap) {
+		err = -ENOMEM;
+		goto free_outmem;
+	}
+
 free_outmem:
 	kvfree(out);
 	return err;
@@ -60,4 +70,129 @@ void mlx5_sf_table_cleanup(struct mlx5_core_dev *dev,
 			   struct mlx5_sf_table *sf_table)
 {
 	mutex_destroy(&sf_table->lock);
+	bitmap_free(sf_table->sf_id_bitmap);
+}
+
+static int mlx5_cmd_alloc_sf(struct mlx5_core_dev *mdev, u16 function_id)
+{
+	u32 out[MLX5_ST_SZ_DW(alloc_sf_out)] = {};
+	u32 in[MLX5_ST_SZ_DW(alloc_sf_in)] = {};
+
+	MLX5_SET(alloc_sf_in, in, opcode, MLX5_CMD_OP_ALLOC_SF);
+	MLX5_SET(alloc_sf_in, in, function_id, function_id);
+
+	return mlx5_cmd_exec(mdev, in, sizeof(in), out, sizeof(out));
+}
+
+static int mlx5_cmd_dealloc_sf(struct mlx5_core_dev *mdev, u16 function_id)
+{
+	u32 out[MLX5_ST_SZ_DW(dealloc_sf_out)] = {};
+	u32 in[MLX5_ST_SZ_DW(dealloc_sf_in)] = {};
+
+	MLX5_SET(dealloc_sf_in, in, opcode, MLX5_CMD_OP_DEALLOC_SF);
+	MLX5_SET(dealloc_sf_in, in, function_id, function_id);
+
+	return mlx5_cmd_exec(mdev, in, sizeof(in), out, sizeof(out));
+}
+
+static const struct devlink_ops sf_devlink_ops = {};
+
+static int alloc_sf_id(struct mlx5_sf_table *sf_table, u16 *sf_id)
+{
+	int ret = 0;
+	u16 idx;
+
+	mutex_lock(&sf_table->lock);
+	idx = find_first_zero_bit(sf_table->sf_id_bitmap, sf_table->max_sfs);
+	if (idx == sf_table->max_sfs) {
+		ret = -ENOSPC;
+		goto done;
+	}
+	bitmap_set(sf_table->sf_id_bitmap, idx, 1);
+	*sf_id = idx;
+done:
+	mutex_unlock(&sf_table->lock);
+	return ret;
+}
+
+static void free_sf_id(struct mlx5_sf_table *sf_table, u16 sf_id)
+{
+	mutex_lock(&sf_table->lock);
+	bitmap_clear(sf_table->sf_id_bitmap, sf_id, 1);
+	mutex_unlock(&sf_table->lock);
+}
+
+static u16 mlx5_sf_hw_id(const struct mlx5_core_dev *coredev, u16 sf_id)
+{
+	return mlx5_sf_base_id(coredev) + sf_id;
+}
+
+/* Perform SF allocation using parent device BAR. */
+struct mlx5_sf *
+mlx5_sf_alloc(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
+	      struct device *dev)
+{
+	struct devlink *devlink;
+	phys_addr_t base_addr;
+	struct mlx5_sf *sf;
+	u16 hw_function_id;
+	u16 sf_id;
+	int ret;
+
+	devlink = devlink_alloc(&sf_devlink_ops, sizeof(*sf));
+	if (!devlink)
+		return ERR_PTR(-ENOMEM);
+
+	ret = alloc_sf_id(sf_table, &sf_id);
+	if (ret)
+		goto id_err;
+
+	hw_function_id = mlx5_sf_hw_id(coredev, sf_id);
+	ret = mlx5_cmd_alloc_sf(coredev, hw_function_id);
+	if (ret)
+		goto alloc_sf_err;
+
+	ret = mlx5_core_enable_sf_hca(coredev, hw_function_id);
+	if (ret)
+		goto enable_err;
+
+	sf = devlink_priv(devlink);
+	sf->idx = sf_id;
+	sf->dev.device = dev;
+	sf->dev.pdev = coredev->pdev;
+	sf->dev.coredev_type = MLX5_COREDEV_SF;
+	base_addr = sf_table->base_address +
+			(sf_id << (sf_table->log_sf_bar_size + 12));
+	sf->dev.bar_addr = base_addr;
+	sf->dev.iseg_base = base_addr;
+
+	ret = devlink_register(devlink, dev);
+	if (ret)
+		goto devlink_err;
+
+	return sf;
+
+devlink_err:
+	mlx5_core_disable_sf_hca(coredev, hw_function_id);
+enable_err:
+	mlx5_cmd_dealloc_sf(coredev, hw_function_id);
+alloc_sf_err:
+	free_sf_id(sf_table, sf_id);
+id_err:
+	devlink_free(devlink);
+	return ERR_PTR(ret);
+}
+
+void mlx5_sf_free(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
+		  struct mlx5_sf *sf)
+{
+	struct devlink *devlink = priv_to_devlink(sf);
+	u16 hw_function_id;
+
+	hw_function_id = mlx5_sf_hw_id(coredev, sf->idx);
+	devlink_unregister(devlink);
+	mlx5_core_disable_sf_hca(coredev, hw_function_id);
+	mlx5_cmd_dealloc_sf(coredev, hw_function_id);
+	free_sf_id(sf_table, sf->idx);
+	devlink_free(devlink);
 }

drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.h
@@ -7,10 +7,16 @@
 #include <linux/mlx5/driver.h>
 #include <linux/mlx5/eswitch.h>
 
+struct mlx5_sf {
+	struct mlx5_core_dev dev;
+	u16 idx;	/* Index allocated by the SF table bitmap */
+};
+
 struct mlx5_sf_table {
 	phys_addr_t base_address;
 	/* Protects sfs life cycle and sf enable/disable flows */
 	struct mutex lock;
+	unsigned long *sf_id_bitmap;
 	u16 max_sfs;
 	u16 log_sf_bar_size;
 };
@@ -22,11 +28,22 @@ static inline bool mlx5_core_is_sf_supported(const struct mlx5_core_dev *dev)
 	       MLX5_CAP_GEN(dev, sf);
 }
 
+static inline u16 mlx5_sf_base_id(const struct mlx5_core_dev *dev)
+{
+	return MLX5_CAP_GEN(dev, sf_base_id);
+}
+
 #ifdef CONFIG_MLX5_MDEV
 int mlx5_sf_table_init(struct mlx5_core_dev *dev,
 		       struct mlx5_sf_table *sf_table);
 void mlx5_sf_table_cleanup(struct mlx5_core_dev *dev,
 			   struct mlx5_sf_table *sf_table);
+
+struct mlx5_sf *
+mlx5_sf_alloc(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
+	      struct device *dev);
+void mlx5_sf_free(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
+		  struct mlx5_sf *sf);
 #endif
 
 #endif

drivers/net/ethernet/mellanox/mlx5/core/cmd.c
@@ -325,6 +325,7 @@ static int mlx5_internal_err_ret_value(struct mlx5_core_dev *dev, u16 op,
 	case MLX5_CMD_OP_PAGE_FAULT_RESUME:
 	case MLX5_CMD_OP_DESTROY_CAPI_PEC:
 	case MLX5_CMD_OP_QUERY_ESW_FUNCTIONS:
+	case MLX5_CMD_OP_DEALLOC_SF:
 	case MLX5_CMD_OP_QUERY_DIAGNOSTIC_PARAMS:
 	case MLX5_CMD_OP_SET_DIAGNOSTIC_PARAMS:
 	case MLX5_CMD_OP_QUERY_DIAGNOSTIC_COUNTERS:
@@ -464,6 +465,7 @@ static int mlx5_internal_err_ret_value(struct mlx5_core_dev *dev, u16 op,
 	case MLX5_CMD_OP_DESTROY_UMEM:
 	case MLX5_CMD_OP_ALLOC_MEMIC:
 	case MLX5_CMD_OP_QUERY_SF_PARTITION:
+	case MLX5_CMD_OP_ALLOC_SF:
 	case MLX5_CMD_OP_SYNC_STEERING:
 		*status = MLX5_DRIVER_STATUS_ABORTED;
 		*synd = MLX5_DRIVER_SYND;
@@ -492,6 +494,8 @@ const char *mlx5_command_str(int command)
 	MLX5_COMMAND_STR_CASE(SET_ISSI);
 	MLX5_COMMAND_STR_CASE(SET_DRIVER_VERSION);
 	MLX5_COMMAND_STR_CASE(QUERY_SF_PARTITION);
+	MLX5_COMMAND_STR_CASE(ALLOC_SF);
+	MLX5_COMMAND_STR_CASE(DEALLOC_SF);
 	MLX5_COMMAND_STR_CASE(CREATE_MKEY);
 	MLX5_COMMAND_STR_CASE(QUERY_MKEY);
 	MLX5_COMMAND_STR_CASE(DESTROY_MKEY);

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -827,28 +827,51 @@ static int mlx5_core_set_hca_defaults(struct mlx5_core_dev *dev)
 	return ret;
 }
 
-int mlx5_core_enable_hca(struct mlx5_core_dev *dev, u16 func_id)
+static int enable_hca(struct mlx5_core_dev *dev, u16 func_id, bool ecpu)
 {
-	u32 out[MLX5_ST_SZ_DW(enable_hca_out)] = {0};
-	u32 in[MLX5_ST_SZ_DW(enable_hca_in)]   = {0};
+	u32 out[MLX5_ST_SZ_DW(enable_hca_out)] = {};
+	u32 in[MLX5_ST_SZ_DW(enable_hca_in)] = {};
 
 	MLX5_SET(enable_hca_in, in, opcode, MLX5_CMD_OP_ENABLE_HCA);
 	MLX5_SET(enable_hca_in, in, function_id, func_id);
-	MLX5_SET(enable_hca_in, in, embedded_cpu_function,
-		 dev->caps.embedded_cpu);
+	MLX5_SET(enable_hca_in, in, embedded_cpu_function, ecpu);
 	return mlx5_cmd_exec(dev, &in, sizeof(in), &out, sizeof(out));
+}
+
+int mlx5_core_enable_hca(struct mlx5_core_dev *dev, u16 func_id)
+{
+	return enable_hca(dev, func_id, dev->caps.embedded_cpu);
+}
+
+int mlx5_core_enable_sf_hca(struct mlx5_core_dev *dev, u16 sf_func_id)
+{
+	/* When enabling SF, it doesn't matter if is enabled on ECPF or PF,
+	 * embedded_cpu bit must be cleared as expected by device firmware.
+	 * SF function ids are split between ECPF And PF. A given SF is for
+	 * ECPF or for PF is decided by SF's function id by the firmware.
+	 */
+	return enable_hca(dev, sf_func_id, 0);
+}
+
+static int disable_hca(struct mlx5_core_dev *dev, u16 func_id, bool ecpu)
+{
+	u32 out[MLX5_ST_SZ_DW(disable_hca_out)] = {};
+	u32 in[MLX5_ST_SZ_DW(disable_hca_in)] = {};
+
+	MLX5_SET(disable_hca_in, in, opcode, MLX5_CMD_OP_DISABLE_HCA);
+	MLX5_SET(disable_hca_in, in, function_id, func_id);
+	MLX5_SET(enable_hca_in, in, embedded_cpu_function, ecpu);
+	return mlx5_cmd_exec(dev, in, sizeof(in), out, sizeof(out));
 }
 
 int mlx5_core_disable_hca(struct mlx5_core_dev *dev, u16 func_id)
 {
-	u32 out[MLX5_ST_SZ_DW(disable_hca_out)] = {0};
-	u32 in[MLX5_ST_SZ_DW(disable_hca_in)]   = {0};
+	return disable_hca(dev, func_id, dev->caps.embedded_cpu);
+}
 
-	MLX5_SET(disable_hca_in, in, opcode, MLX5_CMD_OP_DISABLE_HCA);
-	MLX5_SET(disable_hca_in, in, function_id, func_id);
-	MLX5_SET(enable_hca_in, in, embedded_cpu_function,
-		 dev->caps.embedded_cpu);
-	return mlx5_cmd_exec(dev, in, sizeof(in), out, sizeof(out));
+int mlx5_core_disable_sf_hca(struct mlx5_core_dev *dev, u16 sf_func_id)
+{
+	return disable_hca(dev, sf_func_id, 0);
 }
 
 u64 mlx5_read_internal_timer(struct mlx5_core_dev *dev,

drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
@@ -230,6 +230,8 @@ int mlx5_create_vf_group_sysfs(struct mlx5_core_dev *dev,
 void mlx5_destroy_vf_group_sysfs(struct mlx5_core_dev *dev,
 				 struct kobject *group_kobj);
 int mlx5_core_disable_hca(struct mlx5_core_dev *dev, u16 func_id);
+int mlx5_core_enable_sf_hca(struct mlx5_core_dev *dev, u16 sf_func_id);
+int mlx5_core_disable_sf_hca(struct mlx5_core_dev *dev, u16 sf_func_id);
 int mlx5_create_scheduling_element_cmd(struct mlx5_core_dev *dev, u8 hierarchy,
 				       void *context, u32 *element_id);
 int mlx5_modify_scheduling_element_cmd(struct mlx5_core_dev *dev, u8 hierarchy,
@@ -414,6 +416,8 @@ int set_tunneled_operation(struct mlx5_core_dev *mdev,
 			   u64 *response_bar_address);
 
 #ifdef CONFIG_MLX5_MDEV
+struct mlx5_sf;
+
 u16 mlx5_core_max_sfs(const struct mlx5_core_dev *dev);
 u16 mlx5_get_free_sfs(struct mlx5_core_dev *dev);
 int mlx5_sf_get_mac(struct mlx5_sf *sf, u8 *mac);

include/linux/mlx5/driver.h
@@ -207,7 +207,8 @@ enum port_state_policy {
 
 enum mlx5_coredev_type {
 	MLX5_COREDEV_PF,
-	MLX5_COREDEV_VF
+	MLX5_COREDEV_VF,
+	MLX5_COREDEV_SF
 };
 
 struct mlx5_field_desc {
@@ -1275,6 +1276,11 @@ enum {
 static inline bool mlx5_core_is_pf(const struct mlx5_core_dev *dev)
 {
 	return dev->coredev_type == MLX5_COREDEV_PF;
+}
+
+static inline bool mlx5_core_is_sf(const struct mlx5_core_dev *dev)
+{
+	return dev->coredev_type == MLX5_COREDEV_SF;
 }
 
 static inline bool mlx5_core_is_ecpf(struct mlx5_core_dev *dev)

sirdarckcat/ubuntu

commit 94166c67ff66b30f33eb18723e6e5f88e0a068be

UBUNTU: SAUCE: net/mlx5: Fix mdev create - devlink deadlock
The following scenario leads to a deadlock:

1. sf create via sysfs
     mlx5_mdev_create
       down_read(&table->cleanup_rwsem);
   	 mlx5_sf_alloc
	   devlink_register
             mutex_lock(&devlink_mutex);

2. Set E-switch mode via devlink
     mutex_lock(&devlink_mutex);
      esw_offloads_enable
	mlx5_meddev_init
	  down_read(&table->cleanup_rwsem);

To fix this deadlock, move devlink registration to the mdev driver.

Fixes: df7a26dbb4ff ("net/mlx5: Introduce SF life cycle APIs")
Signed-off-by: Yuval Avnery <yuvalav@mellanox.com>
Signed-off-by: Parav Pandit <parav@mellanox.com>
Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

drivers/net/ethernet/mellanox/mlx5/core/meddev/mdev_driver.c
@@ -8,20 +8,48 @@
 #include "mlx5_core.h"
 #include "meddev/sf.h"
 
+static const struct devlink_ops sf_devlink_ops = {};
+
 static int mlx5_meddev_probe(struct device *dev)
 {
 	struct mdev_device *meddev = mdev_from_dev(dev);
 	struct mlx5_sf *sf = mdev_get_drvdata(meddev);
+	struct mlx5_core_dev *coredev;
+	struct devlink *devlink;
+	int ret;
 
-	return mlx5_sf_load(sf);
+	devlink = devlink_alloc(&sf_devlink_ops, sizeof(*coredev));
+	if (!devlink)
+		return -ENOMEM;
+
+	coredev = devlink_priv(devlink);
+	coredev->device = dev;
+	coredev->pdev = sf->parent_dev->pdev;
+	coredev->bar_addr = sf->bar_base_addr;
+	coredev->iseg_base = sf->bar_base_addr;
+	coredev->coredev_type = MLX5_COREDEV_SF;
+
+	sf->dev = coredev;
+	ret = mlx5_sf_load(sf);
+	if (ret)
+		goto load_err;
+
+	return 0;
+
+load_err:
+	devlink_free(devlink);
+	return ret;
 }
 
 static void mlx5_meddev_remove(struct device *dev)
 {
 	struct mdev_device *meddev = mdev_from_dev(dev);
 	struct mlx5_sf *sf = mdev_get_drvdata(meddev);
+	struct devlink *devlink;
 
+	devlink = priv_to_devlink(sf->dev);
 	mlx5_sf_unload(sf);
+	devlink_free(devlink);
 }
 
 static struct mdev_driver mlx5_meddev_driver = {

drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.c
@@ -10,6 +10,7 @@
 #include "sf.h"
 #include "mlx5_core.h"
 #include "eswitch.h"
+#include "devlink.h"
 
 static int
 mlx5_cmd_query_sf_partitions(struct mlx5_core_dev *mdev, u32 *out, int outlen)
@@ -96,8 +97,6 @@ static int mlx5_cmd_dealloc_sf(struct mlx5_core_dev *mdev, u16 function_id)
 	return mlx5_cmd_exec(mdev, in, sizeof(in), out, sizeof(out));
 }
 
-static const struct devlink_ops sf_devlink_ops = {};
-
 static int alloc_sf_id(struct mlx5_sf_table *sf_table, u16 *sf_id)
 {
 	int ret = 0;
@@ -137,15 +136,14 @@ struct mlx5_sf *
 mlx5_sf_alloc(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
 	      struct device *dev)
 {
-	struct devlink *devlink;
 	phys_addr_t base_addr;
 	struct mlx5_sf *sf;
 	u16 hw_function_id;
 	u16 sf_id;
 	int ret;
 
-	devlink = devlink_alloc(&sf_devlink_ops, sizeof(*sf));
-	if (!devlink)
+	sf = kzalloc(sizeof(*sf), GFP_KERNEL);
+	if (!sf)
 		return ERR_PTR(-ENOMEM);
 
 	ret = alloc_sf_id(sf_table, &sf_id);
@@ -165,25 +163,13 @@ mlx5_sf_alloc(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
 	if (ret)
 		goto vport_err;
 
-	sf = devlink_priv(devlink);
 	sf->idx = sf_id;
 	sf->parent_dev = coredev;
-	sf->dev.device = dev;
-	sf->dev.pdev = coredev->pdev;
-	sf->dev.coredev_type = MLX5_COREDEV_SF;
 	base_addr = sf_table->base_address +
 			(sf_id << (sf_table->log_sf_bar_size + 12));
-	sf->dev.bar_addr = base_addr;
-	sf->dev.iseg_base = base_addr;
-
-	ret = devlink_register(devlink, dev);
-	if (ret)
-		goto devlink_err;
-
+	sf->bar_base_addr = base_addr;
 	return sf;
 
-devlink_err:
-	mlx5_eswitch_cleanup_sf_vport(coredev->priv.eswitch, hw_function_id);
 vport_err:
 	mlx5_core_disable_sf_hca(coredev, hw_function_id);
 enable_err:
@@ -191,23 +177,21 @@ enable_err:
 alloc_sf_err:
 	free_sf_id(sf_table, sf_id);
 id_err:
-	devlink_free(devlink);
+	kfree(sf);
 	return ERR_PTR(ret);
 }
 
 void mlx5_sf_free(struct mlx5_core_dev *coredev, struct mlx5_sf_table *sf_table,
 		  struct mlx5_sf *sf)
 {
-	struct devlink *devlink = priv_to_devlink(sf);
 	u16 hw_function_id;
 
 	hw_function_id = mlx5_sf_hw_id(coredev, sf->idx);
-	devlink_unregister(devlink);
 	mlx5_eswitch_cleanup_sf_vport(coredev->priv.eswitch, hw_function_id);
 	mlx5_core_disable_sf_hca(coredev, hw_function_id);
 	mlx5_cmd_dealloc_sf(coredev, hw_function_id);
 	free_sf_id(sf_table, sf->idx);
-	devlink_free(devlink);
+	kfree(sf);
 }
 
 u16 mlx5_get_free_sfs_locked(struct mlx5_core_dev *dev,
@@ -397,7 +381,7 @@ static void set_dma_params(struct mlx5_core_dev *coredev, struct device *dev)
 
 int mlx5_sf_load(struct mlx5_sf *sf)
 {
-	struct mlx5_core_dev *dev = &sf->dev;
+	struct mlx5_core_dev *dev = sf->dev;
 	struct mlx5_core_dev *parent_dev;
 	int err;
 
@@ -433,9 +417,36 @@ mdev_err:
 
 void mlx5_sf_unload(struct mlx5_sf *sf)
 {
-	mlx5_unload_one(&sf->dev, true);
-	mlx5_mdev_uninit(&sf->dev);
-	iounmap(sf->dev.iseg);
+	{
+	/* Yuk, This is done because remove_one()
+	 * invokes devlink_unregister() which must be mirror
+	 * of mlx5_load_one() which does devlink_register().
+	 * Code normally should be,
+	 * mlx5_load_one()
+	 *    devlink_register()
+	 *
+	 * mlx5_unload_one()
+	 *    devlin_unregister()
+	 * This is done incorrectly in upstream currently.
+	 * Correcting it, will result into lock dependency asserts
+	 * and deadlock described in
+	 * http://l-gerrit.mtl.labs.mlnx:8080/#/c/upstream/linux/+/281516/
+	 *
+	 * Assert on load_one() can be ignored because its false assert.
+	 * Its false because, devlink cannot reload a devlink device which
+	 * hasn't yet done devlink_register() while holding
+	 * interface_state_mutex.
+	 * To avoid this devlink user space commands must not take
+	 * devlink lock as proposed.
+	 */
+		struct devlink *devlink = priv_to_devlink(sf->dev);
+
+		mlx5_devlink_unregister(devlink);
+	}
+
+	mlx5_unload_one(sf->dev, true);
+	mlx5_mdev_uninit(sf->dev);
+	iounmap(sf->dev->iseg);
 }
 
 int mlx5_sf_set_mac(struct mlx5_sf *sf, u8 *mac)

drivers/net/ethernet/mellanox/mlx5/core/meddev/sf.h
@@ -6,10 +6,12 @@
 
 #include <linux/mlx5/driver.h>
 #include <linux/mlx5/eswitch.h>
+#include <linux/mdev.h>
 
 struct mlx5_sf {
-	struct mlx5_core_dev dev;
+	struct mlx5_core_dev *dev;
 	struct mlx5_core_dev *parent_dev;
+	phys_addr_t bar_base_addr;
 	u16 idx;	/* Index allocated by the SF table bitmap */
 };
 
@@ -55,6 +57,15 @@ int mlx5_sf_set_max_sfs(struct mlx5_core_dev *dev,
 int mlx5_sf_load(struct mlx5_sf *sf);
 void mlx5_sf_unload(struct mlx5_sf *sf);
 
+static inline struct mlx5_core_dev *
+mlx5_sf_get_parent_dev(struct mlx5_core_dev *dev)
+{
+	struct mdev_device *meddev = mdev_from_dev(dev->device);
+	struct mlx5_sf *sf = mdev_get_drvdata(meddev);
+
+	return sf->parent_dev;
+}
+
 #else
 static inline u16 mlx5_core_max_sfs(const struct mlx5_core_dev *dev,
 				    struct mlx5_sf_table *sf_table)
@@ -68,14 +79,12 @@ static inline u16 mlx5_get_free_sfs(struct mlx5_core_dev *dev,
 	return 0;
 }
 
-#endif
-
 static inline struct mlx5_core_dev *
 mlx5_sf_get_parent_dev(struct mlx5_core_dev *dev)
 {
-	struct mlx5_sf *sf = container_of(dev, struct mlx5_sf, dev);
-
-	return sf->parent_dev;
+	return NULL;
 }
+
+#endif
 
 #endif