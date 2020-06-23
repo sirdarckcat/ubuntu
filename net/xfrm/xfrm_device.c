bcfcc9a10cc7e9d0fd23c9f7b42082e52b07cb83
Jarod Wilson <jarod@redhat.com>
2020-06-23 16:40:01 -0400

    bonding/xfrm: use real_dev instead of slave_dev
    BugLink: https://bugs.launchpad.net/bugs/1926994
    
    Rather than requiring every hw crypto capable NIC driver to do a check for
    slave_dev being set, set real_dev in the xfrm layer and xso init time, and
    then override it in the bonding driver as needed. Then NIC drivers can
    always use real_dev, and at the same time, we eliminate the use of a
    variable name that probably shouldn't have been used in the first place,
    particularly given recent current events.
    
    CC: Boris Pismenny <borisp@mellanox.com>
    CC: Saeed Mahameed <saeedm@mellanox.com>
    CC: Leon Romanovsky <leon@kernel.org>
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Veaceslav Falico <vfalico@gmail.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    CC: "David S. Miller" <davem@davemloft.net>
    CC: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    CC: Jakub Kicinski <kuba@kernel.org>
    CC: Steffen Klassert <steffen.klassert@secunet.com>
    CC: Herbert Xu <herbert@gondor.apana.org.au>
    CC: netdev@vger.kernel.org
    Suggested-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit bdfd2d1fa79acd03e18d1683419572f3682b39fd)
    Signed-off-by: Daniel Jurgens <danielj@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -94,8 +94,8 @@ struct sk_buff *validate_xmit_xfrm(struct sk_buff *skb, netdev_features_t featur
 	if (xo->flags & XFRM_GRO || x->xso.flags & XFRM_OFFLOAD_INBOUND)
 		return skb;
 
-	/* This skb was already validated on the master dev */
-	if ((x->xso.dev != dev) && (x->xso.slave_dev == dev))
+	/* This skb was already validated on the upper/virtual dev */
+	if ((x->xso.dev != dev) && (x->xso.real_dev == dev))
 		return skb;
 
 	local_irq_save(flags);
@@ -245,6 +245,7 @@ int xfrm_dev_state_add(struct net *net, struct xfrm_state *x,
 	}
 
 	xso->dev = dev;
+	xso->real_dev = dev;
 	xso->num_exthdrs = 1;
 	/* Don't forward bit that is not implemented */
 	xso->flags = xuo->flags & ~XFRM_OFFLOAD_IPV6;
