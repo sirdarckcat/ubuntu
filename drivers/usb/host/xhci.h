33974b164afd33cd279986c6715b371e4c9e7f02
Jonathan Bell <jonathan@raspberrypi.org>
2020-10-26 14:03:35 +0000

    xhci: quirks: add link TRB quirk for VL805
    BugLink: https://bugs.launchpad.net/bugs/1908077
    
    The VL805 controller can't cope with the TR Dequeue Pointer for an endpoint
    being set to a Link TRB. The hardware-maintained endpoint context ends up
    stuck at the address of the Link TRB, leading to erroneous ring expansion
    events whenever the enqueue pointer wraps to the dequeue position.
    
    If the search for the end of the current TD and ring cycle state lands on
    a Link TRB, move to the next segment.
    
    See: https://github.com/raspberrypi/linux/issues/3919
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    (backported from commit 473bfd7b545bd88da98ddc63cd4db9013391cfc0 rpi-5.4.y)
    [juergh: Use bit 38 for XHCI_DISABLE_SPARSE to match upstream.]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>
    Signed-off-by: Ian May <ian.may@canonical.com>
    [kleber: bit 37 has been taken by upstream
     f768e718911e03a4a20b65f984eaa9b09045e4cd. User bit 39 instead for
     XHCI_AVOID_DQ_ON_LINK.]
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1885,6 +1885,7 @@ struct xhci_hcd {
 #define XHCI_SKIP_PHY_INIT	BIT_ULL(37)
 #define XHCI_DISABLE_SPARSE	BIT_ULL(38)
 #define XHCI_NO_SOFT_RETRY	BIT_ULL(40)
+#define XHCI_AVOID_DQ_ON_LINK	BIT_ULL(39)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
