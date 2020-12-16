fce879658a2c642665ed01777222562b7cf113be
Kai-Heng Feng <kai.heng.feng@canonical.com>
2020-12-16 17:10:03 +0800

    UBUNTU: SAUCE: net: macb: Consolidate wakeup settings onto platform device
    BugLink: https://launchpad.net/bugs/1895637 [System could not be woken up by WOL from S3 or S5]
    
    There are two devices' wakeup get changed by the driver, one is on the
    platform device (i.e. pdev->dev), another one is netdev device
    (i.e. bp->dev->dev).
    
    In principle we should use the top level device, which in this case is
    platform device, for wakeup purpose. Because in general the top level
    device is the one with real wakeup capability.
    
    So consolidate all wakeup routines to use platform device.
    
    Note that after this change, this sysfs will disappear:
    /sys/devices/platform/amba/ff0e0000.ethernet/net/eth0/power/wakeup
    
    We should use this instead:
    /sys/devices/platform/amba/ff0e0000.ethernet/power/wakeup
    
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -4500,7 +4500,7 @@ static int macb_probe(struct platform_device *pdev)
 		     (unsigned long)bp);
 
 	if (bp->caps & MACB_CAPS_WOL)
-		device_set_wakeup_capable(&bp->dev->dev, 1);
+		device_set_wakeup_capable(&pdev->dev, 1);
 
 	phy_attached_info(phydev);
 
@@ -4581,7 +4581,7 @@ static int __maybe_unused macb_suspend(struct device *dev)
 	if (!netif_running(netdev))
 		return 0;
 
-	if (device_may_wakeup(&bp->dev->dev)) {
+	if (device_may_wakeup(dev)) {
 		spin_lock_irqsave(&bp->lock, flags);
 		ctrl = macb_readl(bp, NCR);
 		ctrl &= ~(MACB_BIT(TE) | MACB_BIT(RE));
@@ -4663,7 +4663,7 @@ static int __maybe_unused macb_resume(struct device *dev)
 	if (!device_may_wakeup(dev))
 		pm_runtime_force_resume(dev);
 
-	if (device_may_wakeup(&bp->dev->dev)) {
+	if (device_may_wakeup(dev)) {
 		spin_lock_irqsave(&bp->lock, flags);
 		macb_writel(bp, IDR, MACB_BIT(WOL));
 		gem_writel(bp, WOL, 0);
@@ -4717,7 +4717,7 @@ static int __maybe_unused macb_runtime_suspend(struct device *dev)
 		clk_disable_unprepare(bp->rx_clk);
 	}
 
-	if (!(device_may_wakeup(&bp->dev->dev) &&
+	if (!(device_may_wakeup(dev) &&
 	      (bp->caps & MACB_CAPS_NEED_TSUCLK)))
 		clk_disable_unprepare(bp->tsu_clk);
 
@@ -4736,7 +4736,7 @@ static int __maybe_unused macb_runtime_resume(struct device *dev)
 		clk_prepare_enable(bp->rx_clk);
 	}
 
-	if (!(device_may_wakeup(&bp->dev->dev) &&
+	if (!(device_may_wakeup(dev) &&
 	      (bp->caps & MACB_CAPS_NEED_TSUCLK)))
 		clk_prepare_enable(bp->tsu_clk);
 
