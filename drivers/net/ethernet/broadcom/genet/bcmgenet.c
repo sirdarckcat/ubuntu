sirdarckcat/ubuntu

commit dc92fc64c5c32ffd43722a0f8e48e2e2e4facfee

Revert "net: bcmgenet: Workaround for Pi 4B network issue"
BugLink: https://bugs.launchpad.net/bugs/1882930

This reverts commit 55d2b00e1b685d263f6057f441903939f9da1c61.

Signed-off-by: Juerg Haefliger <juergh@canonical.com>
Acked-by: Stefan Bader <stefan.bader@canonical.com>
Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

drivers/net/ethernet/broadcom/genet/bcmgenet.c
@@ -72,10 +72,6 @@
 /* Forward declarations */
 static void bcmgenet_set_rx_mode(struct net_device *dev);
 
-static bool force_reneg = false;
-module_param(force_reneg, bool, 0444);
-MODULE_PARM_DESC(force_reneg, "Force a renegotiation after the initial link-up");
-
 static bool skip_umac_reset = true;
 module_param(skip_umac_reset, bool, 0444);
 MODULE_PARM_DESC(skip_umac_reset, "Skip UMAC reset step");
@@ -2620,7 +2616,6 @@ static void bcmgenet_irq_task(struct work_struct *work)
 	unsigned int status;
 	struct bcmgenet_priv *priv = container_of(
 			work, struct bcmgenet_priv, bcmgenet_irq_work);
-	static int first_link = 1;
 
 	netif_dbg(priv, intr, priv->dev, "%s\n", __func__);
 
@@ -2636,27 +2631,9 @@ static void bcmgenet_irq_task(struct work_struct *work)
 	}
 
 	/* Link UP/DOWN event */
-	if (status & UMAC_IRQ_LINK_EVENT) {
-		priv->dev->phydev->link = !!(status & UMAC_IRQ_LINK_UP);
+	if (status & UMAC_IRQ_LINK_EVENT)
 		phy_mac_interrupt(priv->dev->phydev);
 
-		if (priv->dev->phydev->link && first_link) {
-			first_link = 0;
-			/*
-			 * HACK: Some Pi4Bs, when paired with some switches,
-			 * come up in a strange state where they are unable to
-			 * transmit, causing them to fail to get an IP address.
-			 * Although the failure mechanism is not yet understood,
-			 * forcing renegotiation at this point has been shown
-			 * to be effective in avoiding the problem.
-			 */
-			if (force_reneg) {
-				dev_info(&priv->pdev->dev,
-					 "Forcing renegotiation\n");
-				genphy_restart_aneg(priv->dev->phydev);
-			}
-		}
-	}
 }
 
 /* bcmgenet_isr1: handle Rx and Tx priority queues */

commit ef5d6569735dc0183167198ec0603fe6e3b0296b

UBUNTU: SAUCE: net: bcmgenet: Remove empty line to match upstream
BugLink: https://bugs.launchpad.net/bugs/1908077

Drop a stray empty line to match upstream raspberrypi.

Fixes: dc92fc64c5c3 ("Revert "net: bcmgenet: Workaround for Pi 4B network issue"")
Signed-off-by: Juerg Haefliger <juergh@canonical.com>
Signed-off-by: Ian May <ian.may@canonical.com>

drivers/net/ethernet/broadcom/genet/bcmgenet.c
@@ -71,7 +71,6 @@
 
 /* Forward declarations */
 static void bcmgenet_set_rx_mode(struct net_device *dev);
-
 static bool skip_umac_reset = true;
 module_param(skip_umac_reset, bool, 0444);
 MODULE_PARM_DESC(skip_umac_reset, "Skip UMAC reset step");