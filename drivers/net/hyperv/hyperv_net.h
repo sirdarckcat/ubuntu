3d3535dafad335fc0f5b26577faa30fa6856bbed
Tianyu Lan <Tianyu.Lan@microsoft.com>
2021-02-10 07:05:21 -0800

    UBUNTU: SAUCE: HV/Netvsc: Unmap recv_buf and send buf in extra address space
    BugLink: https://bugs.launchpad.net/bugs/1915146
    
    This patch is to unmap recv/send gpadl buffer's va in extra address
    space and free these pages with correct point when free netvsc device.
    
    Signed-off-by: Tianyu Lan <Tianyu.Lan@microsoft.com>
    (cherry picked from commit https://github.com/lantianyu/linux/commit/80ab9dd6c72c888faa03e0a26c79c068afdac843)
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -1073,6 +1073,7 @@ struct netvsc_device {
 
 	/* Receive buffer allocated by us but manages by NetVSP */
 	void *recv_buf;
+	void *recv_original_buf;
 	u32 recv_buf_size; /* allocated bytes */
 	u32 recv_buf_gpadl_handle;
 	u32 recv_section_cnt;
@@ -1081,7 +1082,9 @@ struct netvsc_device {
 
 	/* Send buffer allocated by us */
 	void *send_buf;
+	void *send_original_buf;
 	u32 send_buf_gpadl_handle;
+	u32 send_buf_size;
 	u32 send_section_cnt;
 	u32 send_section_size;
 	unsigned long *send_section_map;
