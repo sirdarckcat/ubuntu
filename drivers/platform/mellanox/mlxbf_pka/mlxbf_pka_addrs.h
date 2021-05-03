1e6645aa38d93f2712200c5d75d9f4d23d61b825
Mahantesh Salimath <mahantesh@nvidia.com>
2021-05-03 12:57:22 -0400

    UBUNTU: SAUCE: pka: Enable DRBG block in TRNG
    BugLink: https://bugs.launchpad.net/bugs/1926773
    
    * DRBG should be enabled for FIPS compliance. Besides,
      it makes TRNG more robust due to the AES core present inside.
    
    * DRBG should be reseeded often and currently it is configured to reseed
      after generating 256 blocks of 128-bit random output.
      DRBG is used in conjunction with Conditioning Functioning and without
      BC_DF block. Therefore, random output is not blocked during reseed operation.
    
    * Before using TRNG with DRBG configuration, NIST known answer test is performed
      on the entire DRBG block to verify if DRBG is functioning as expected.
    
    * Personalization string for DRBG is chosen so that it fits into the 12 registers (384 bits).
    
    * 'trng_read' local variable needs to be cleared after acknowledging the random
      output everytime. Else, it is always set and old random data might be used in certain cases.
    
    Signed-off-by: Mahantesh Salimath <mahantesh@nvidia.com>
    Reviewed-by: Khalil Blaiech <kblaiech@nvidia.com>
    Signed-off-by: Mahantesh Salimath <mahantesh@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/platform/mellanox/mlxbf_pka/mlxbf_pka_addrs.h
+++ drivers/platform/mellanox/mlxbf_pka/mlxbf_pka_addrs.h
@@ -117,6 +117,19 @@
 #define TRNG_POKER_B_8          0x120D0
 #define TRNG_POKER_F_C          0x120D8
 
+#define TRNG_PS_AI_0_ADDR       0x12080
+#define TRNG_PS_AI_1_ADDR       0x12088
+#define TRNG_PS_AI_2_ADDR       0x12090
+#define TRNG_PS_AI_3_ADDR       0x12098
+#define TRNG_PS_AI_4_ADDR       0x120A0
+#define TRNG_PS_AI_5_ADDR       0x120A8
+#define TRNG_PS_AI_6_ADDR       0x120B0
+#define TRNG_PS_AI_7_ADDR       0x120B8
+#define TRNG_PS_AI_8_ADDR       0x120C0
+#define TRNG_PS_AI_9_ADDR       0x120C8
+#define TRNG_PS_AI_10_ADDR      0x120D0
+#define TRNG_PS_AI_11_ADDR      0x120D8
+
 // Control register address/offset. This is accessed from the ARM using 8
 // byte reads/writes however only the bottom 32 bits are implemented.
 #define PKA_MASTER_SEQ_CTRL_ADDR    0x27F90
