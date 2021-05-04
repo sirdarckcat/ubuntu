a46cac7bb6dc919221b8d0ebff5e7141ca373ac6
David Thompson <davthompson@nvidia.com>
2021-05-04 15:22:45 -0400

    UBUNTU: SAUCE: mlxbf_gige: syncup with v1.21 content
    BugLink: https://bugs.launchpad.net/bugs/1927134
    
    This delivery provides a syncup of the mlxbf_gige driver
    with the Mellanox-internal v1.21 content.
    
    a) v1.20 changes - properly handle negotiated pause settings
       This delivery updates the logic in mlxbf_gige_adjust_link()
       to store the negotiated pause settings into the driver's
       private settings. The pause settings, as reported by the
       PHY device when link comes up, are thereafter returned in
       the get_pauseparam() driver callback.
    
    b) v1.21 changes - modify copyright notice from Nvidia back to Mellanox
    
    Reviewed-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Signed-off-by: David Thompson <davthompson@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_regs.h
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_regs.h
@@ -2,7 +2,7 @@
 
 /* Header file for Mellanox BlueField GigE register defines
  *
- * Copyright (c) 2020-2021 NVIDIA Corporation.
+ * Copyright (C) 2020-2021 Mellanox Technologies, Ltd. ALL RIGHTS RESERVED.
  */
 
 #ifndef __MLXBF_GIGE_REGS_H__
