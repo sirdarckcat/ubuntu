8adca3ee1ccda0e243632e9f71b524303410b4ca
Jin Yao <yao.jin@linux.intel.com>
2021-05-11 15:11:33 +0800

    UBUNTU: SAUCE: perf stat: Support metrics with hybrid events
    BugLink: https://bugs.launchpad.net/bugs/1952356
    
    One metric such as 'Kernel_Utilization' may be from different PMUs and
    consists of different events.
    
    For core,
    Kernel_Utilization = cpu_clk_unhalted.thread:k / cpu_clk_unhalted.thread
    
    For atom,
    Kernel_Utilization = cpu_clk_unhalted.core:k / cpu_clk_unhalted.core
    
    The metric group string is:
    "{cpu_clk_unhalted.thread:k,cpu_clk_unhalted.thread}:W,{cpu_clk_unhalted.core:k,cpu_clk_unhalted.core}:W"
    
    It's internally expanded to:
    "{cpu_clk_unhalted.thread:k,cpu_clk_unhalted.thread}:W#cpu_core,{cpu_clk_unhalted.core:k,cpu_clk_unhalted.core}:W#cpu_atom"
    
    That means the group "{cpu_clk_unhalted.thread:k,cpu_clk_unhalted.thread}:W"
    is from cpu_core PMU and the group "{cpu_clk_unhalted.core:k,cpu_clk_unhalted.core}"
    is from cpu_atom PMU. And then next, checks if the events in group are
    valid on that PMU. If one event is not valid on that PMU, the associated
    group would be removed internally.
    
    In this example, cpu_clk_unhalted.thread is valid on cpu_core and
    cpu_clk_unhalted.core is valid on cpu_atom. So the checks for these two
    groups are passed.
    
    Now it reports:
    
      # ./perf stat -M Kernel_Utilization -a -- sleep 1
    
       Performance counter stats for 'system wide':
    
          15,810,857,860      cpu_clk_unhalted.thread:k [cpu_core] #     1.00 Kernel_Utilization
          15,813,185,328      cpu_clk_unhalted.thread [cpu_core]
           6,399,704,051      cpu_clk_unhalted.core:k [cpu_atom] #     1.00 Kernel_Utilization
           6,399,705,409      cpu_clk_unhalted.core [cpu_atom]
    
           1.001718237 seconds time elapsed
    
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    (back ported from commit 697ba2e8e7b3aac30904aec6ad7e0ced5a0d651c/linux-intel-5.13)
    Signed-off-by: Philip Cox <philip.cox@canonical.com>

--- tools/perf/util/stat-display.c
+++ tools/perf/util/stat-display.c
@@ -538,7 +538,8 @@ static void aggr_update_shadow(struct perf_stat_config *config,
 	}
 }
 
-static void uniquify_event_name(struct evsel *counter)
+static void uniquify_event_name(struct evsel *counter,
+				struct perf_stat_config *stat_config)
 {
 	char *new_name;
 	char *config;
@@ -557,9 +558,12 @@ static void uniquify_event_name(struct evsel *counter)
 			counter->name = new_name;
 		}
 	} else {
-		if (perf_pmu__has_hybrid()) {
-			ret = asprintf(&new_name, "%s/%s/",
-				       counter->pmu_name, counter->name);
+		if (perf_pmu__has_hybrid() &&
+		    stat_config->metric_events.nr_entries == 0) {
+			if (!counter->use_config_name) {
+				ret = asprintf(&new_name, "%s/%s/",
+					       counter->pmu_name, counter->name);
+			}
 		} else {
 			ret = asprintf(&new_name, "%s [%s]",
 				       counter->name, counter->pmu_name);
@@ -620,8 +624,8 @@ static bool collect_data(struct perf_stat_config *config, struct evsel *counter,
 	if (counter->merged_stat)
 		return false;
 	cb(config, counter, data, true);
-	if (config->no_merge || hybrid_uniquify(counter))
-		uniquify_event_name(counter);
+	if (config->no_merge)
+		uniquify_event_name(counter, config);
 	else if (counter->auto_merge_stats || config->hybrid_merge)
 		collect_all_aliases(config, counter, cb, data);
 	return true;
