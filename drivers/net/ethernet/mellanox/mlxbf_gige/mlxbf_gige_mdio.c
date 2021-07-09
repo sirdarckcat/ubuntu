dd58627aa5f14aee549b168ce8c5d8e7e01ad98c
Asmaa Mnebhi <asmaa@nvidia.com>
2021-07-09 11:00:57 -0400

    Revert "UBUNTU: SAUCE: mlxbf_gige: use streaming DMA mapping for packet buffers"
    This reverts commit 9f4f2258bacc13425902ef01c698bf2899091b24.
    
    BugLink: https://bugs.launchpad.net/bugs/1934923
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_mdio.c
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_mdio.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause
-
-/* MDIO support for Mellanox Gigabit Ethernet driver
+/*  MDIO support for Mellanox GigE driver
  *
- * Copyright (c) 2020 NVIDIA Corporation.
+ *  Copyright (c) 2020, NVIDIA Corporation. All rights reserved.
  */
 
 #include <linux/acpi.h>
@@ -68,7 +67,7 @@
 				 FIELD_PREP(MLXBF_GIGE_MDIO_CFG_MDIO3_3_MASK, 1) | \
 				 FIELD_PREP(MLXBF_GIGE_MDIO_CFG_MDIO_FULL_DRIVE_MASK, 1) | \
 				 FIELD_PREP(MLXBF_GIGE_MDIO_CFG_MDC_PERIOD_MASK, \
-					    MLXBF_GIGE_MDIO_PERIOD) |   \
+				 	    MLXBF_GIGE_MDIO_PERIOD) | \
 				 FIELD_PREP(MLXBF_GIGE_MDIO_CFG_MDIO_IN_SAMP_MASK, 6) | \
 				 FIELD_PREP(MLXBF_GIGE_MDIO_CFG_MDIO_OUT_SAMP_MASK, 13))
 
