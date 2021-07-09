cf88a393a46d8a22adfb3192b2f8d838815ef455
Asmaa Mnebhi <asmaa@nvidia.com>
2021-07-09 10:53:05 -0400

    Revert "UBUNTU: SAUCE: mlxbf_gige: syncup with v1.21 content"
    This reverts commit b68212b84f8f9691152dad59d678c70aaf39c9a6.
    
    BugLink: https://bugs.launchpad.net/bugs/1934923
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_rx.c
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_rx.c
@@ -2,7 +2,7 @@
 
 /* Packet receive logic for Mellanox Gigabit Ethernet driver
  *
- * Copyright (C) 2020-2021 Mellanox Technologies, Ltd. ALL RIGHTS RESERVED.
+ * Copyright (c) 2020-2021 NVIDIA Corporation.
  */
 
 #include <linux/etherdevice.h>
