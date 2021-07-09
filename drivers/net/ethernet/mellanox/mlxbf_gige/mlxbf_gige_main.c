df3e55a9ee8725291d6e125d3ae68fd4dc867bcc
Asmaa Mnebhi <asmaa@nvidia.com>
2021-07-09 11:01:25 -0400

    Revert "UBUNTU: SAUCE: mlxbf-gige: remove gpio interrupt coalesce resources"
    This reverts commit d11fc64e9e4bd09536448b4571660e1ad38a6a83.
    
    BugLink: https://bugs.launchpad.net/bugs/1934923
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_main.c
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_main.c
@@ -19,7 +19,7 @@
 #include "mlxbf_gige_regs.h"
 
 #define DRV_NAME    "mlxbf_gige"
-#define DRV_VERSION "1.6"
+#define DRV_VERSION "1.5"
 
 static void mlxbf_gige_set_mac_rx_filter(struct mlxbf_gige *priv,
 					 unsigned int index, u64 dmac)
