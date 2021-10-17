42a105544cf9c3f139146f1b1aa1fd29eca04551
Johannes Berg <johannes.berg@intel.com>
2021-10-17 12:40:12 +0300

    iwlwifi: parse debug exclude data from firmware file
    Parse the memory dump exclusion TLVs from the firmware file
    and save the data to be able to apply them later upon dump.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20211017123741.ec280bcb32cb.Iac0da0e22d7c3c0e237867c9113a06ddd95c7a51@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    (backported from commit fad92a1d11f6ef81cca3c1ea24d5703930339291)
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    (cherry picked from commit 8ebe0a76bb06d44380c5e58bc1b628413656e753/linux-intel-5.13)
    Signed-off-by: Philip Cox <philip.cox@canonical.com>

--- drivers/net/wireless/intel/iwlwifi/fw/img.h
+++ drivers/net/wireless/intel/iwlwifi/fw/img.h
@@ -176,6 +176,10 @@ struct iwl_fw_dbg {
 	u32 dump_mask;
 };
 
+struct iwl_dump_exclude {
+	u32 addr, size;
+};
+
 /**
  * struct iwl_fw - variables associated with the firmware
  *
@@ -196,6 +200,10 @@ struct iwl_fw_dbg {
  * @cipher_scheme: optional external cipher scheme.
  * @human_readable: human readable version
  *	we get the ALIVE from the uCode
+ * @phy_integration_ver: PHY integration version string
+ * @phy_integration_ver_len: length of @phy_integration_ver
+ * @dump_excl: image dump exclusion areas for RT image
+ * @dump_excl_wowlan: image dump exclusion areas for WoWLAN image
  */
 struct iwl_fw {
 	u32 ucode_ver;
@@ -227,6 +235,8 @@ struct iwl_fw {
 
 	u8 *phy_integration_ver;
 	u32 phy_integration_ver_len;
+
+	struct iwl_dump_exclude dump_excl[2], dump_excl_wowlan[2];
 };
 
 static inline const char *get_fw_dbg_mode_string(int mode)
