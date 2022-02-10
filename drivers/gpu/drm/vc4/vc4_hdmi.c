endlessm/linux

commit f601665717451416ed7aa744b527b2e657b57457

drm/vc4: hdmi: Make sure the device is powered with CEC
BugLink: https://bugs.launchpad.net/bugs/1959376

commit 20b0dfa86bef0e80b41b0e5ac38b92f23b6f27f9 upstream.

Similarly to what we encountered with the detect hook with DRM, nothing
actually prevents any of the CEC callback from being run while the HDMI
output is disabled.

However, this is an issue since any register access to the controller
when it's powered down will result in a silent hang.

Let's make sure we run the runtime_pm hooks when the CEC adapter is
opened and closed by the userspace to avoid that issue.

Fixes: 15b4511a4af6 ("drm/vc4: add HDMI CEC support")
Reviewed-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
Signed-off-by: Maxime Ripard <maxime@cerno.tech>
Link: https://patchwork.freedesktop.org/patch/msgid/20210819135931.895976-6-maxime@cerno.tech
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -1735,8 +1735,14 @@ static int vc4_hdmi_cec_adap_enable(struct cec_adapter *adap, bool enable)
 	struct vc4_hdmi *vc4_hdmi = cec_get_drvdata(adap);
 	/* clock period in microseconds */
 	const u32 usecs = 1000000 / CEC_CLOCK_FREQ;
-	u32 val = HDMI_READ(HDMI_CEC_CNTRL_5);
+	u32 val;
+	int ret;
 
+	ret = pm_runtime_resume_and_get(&vc4_hdmi->pdev->dev);
+	if (ret)
+		return ret;
+
+	val = HDMI_READ(HDMI_CEC_CNTRL_5);
 	val &= ~(VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET |
 		 VC4_HDMI_CEC_CNT_TO_4700_US_MASK |
 		 VC4_HDMI_CEC_CNT_TO_4500_US_MASK);
@@ -1881,6 +1887,8 @@ static int vc4_hdmi_cec_init(struct vc4_hdmi *vc4_hdmi)
 	ret = cec_register_adapter(vc4_hdmi->cec_adap, &pdev->dev);
 	if (ret < 0)
 		goto err_remove_handlers;
+
+	pm_runtime_put(&vc4_hdmi->pdev->dev);
 
 	return 0;
 

sirdarckcat/ubuntu

commit d20f5b927819bfed2596e47b7e28824d427a7358

UBUNTU: Fix "drm/vc4: hdmi: Make sure the device is powered with CEC"
BugLink: https://bugs.launchpad.net/bugs/1960323

The referenced stable commit is broken in 5.15.19 and subsequently fixed
in 5.15.21. Add a temporary fix until these commits trickle down.

Fixes: f60166571745 ("drm/vc4: hdmi: Make sure the device is powered with CEC")
Signed-off-by: Juerg Haefliger <juergh@canonical.com>

drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -2533,6 +2533,8 @@ static int vc4_hdmi_cec_disable(struct cec_adapter *adap)
 
 	spin_unlock_irqrestore(&vc4_hdmi->hw_lock, flags);
 
+	pm_runtime_put(&vc4_hdmi->pdev->dev);
+
 	return 0;
 }
 
@@ -2677,8 +2679,6 @@ static int vc4_hdmi_cec_init(struct vc4_hdmi *vc4_hdmi)
 	ret = cec_register_adapter(vc4_hdmi->cec_adap, &pdev->dev);
 	if (ret < 0)
 		goto err_remove_handlers;
-
-	pm_runtime_put(&vc4_hdmi->pdev->dev);
 
 	return 0;
 