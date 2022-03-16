cf44264c5ec2dac3033c5caca9707ded414bfbcf
Maxime Ripard <maxime@cerno.tech>
2022-03-16 09:41:44 +0100

    Revert "clk: requests: Dereference the request pointer after the check"
    BugLink: https://bugs.launchpad.net/bugs/1975599
    
    This reverts commit 12917adc036f82c4fda3b80a068d0d51c947d6a6.
    
    (cherry picked from commit 03adf7626d7d30e6e426b1eb2075c134d59dee33 rpi-5.15.y)
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2683,11 +2683,10 @@ EXPORT_SYMBOL_GPL(clk_request_start);
  */
 void clk_request_done(struct clk_request *req)
 {
-	struct clk_core *core;
+	struct clk_core *core = req->clk->core;
 
 	if (!req)
 		return;
-	core = req->clk->core;
 
 	clk_prepare_lock();
 
