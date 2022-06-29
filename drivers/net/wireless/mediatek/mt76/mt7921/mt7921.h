a87c362a90bf35e8cefa6416a5c88ab42985f617
Deren Wu <deren.wu@mediatek.com>
2022-06-29 16:54:01 +0800

    UBUNTU: SAUCE: mt76: mt7921: fix command timeout in AP stop period
    BugLink: https://bugs.launchpad.net/bugs/1979173
    
    Due to AP stop improperly, mt7921 driver would face random command timeout
    by chip fw problem. Migrate AP start/stop process to .start_ap/.stop_ap and
    congiure BSS network settings in both hooks.
    
    The new flow is shown below.
    * AP start
        .start_ap()
          configure BSS network resource
          set BSS to connected state
        .bss_info_changed()
          enable fw beacon offload
    
    * AP stop
        .bss_info_changed()
          disable fw beacon offload (skip this command)
        .stop_ap()
          set BSS to disconnected state (beacon offload disabled automatically)
          destroy BSS network resource
    
    Fixes: 116c69603b01 ("mt76: mt7921: Add AP mode support")
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Deren Wu <deren.wu@mediatek.com>
    (backported from https://patchwork.kernel.org/project/linux-wireless/patch/21366e8bc70573794b5f12372f2e55e9059d8457.1654691030.git.deren.wu@mediatek.com/)
    [Chris: fix the cherry-pick failure due to incompatible mt7921.h with
    5.17]
    Signed-off-by: Chris Chiu <chris.chiu@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
+++ drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
@@ -299,6 +299,8 @@ int mt7921_run_firmware(struct mt7921_dev *dev);
 int mt7921_mcu_add_key(struct mt7921_dev *dev, struct ieee80211_vif *vif,
 		       struct mt7921_sta *msta, struct ieee80211_key_conf *key,
 		       enum set_key_cmd cmd);
+int mt7921_mcu_set_bss_pm(struct mt7921_dev *dev, struct ieee80211_vif *vif,
+                          bool enable);
 int mt7921_mcu_sta_update(struct mt7921_dev *dev, struct ieee80211_sta *sta,
 			  struct ieee80211_vif *vif, bool enable,
 			  enum mt76_sta_info_state state);
