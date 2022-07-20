97cff2223b3188fcb7605ac13396257c58eb2357
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2022-07-20 19:46:30 +0100

    UBUNTU: Start new release
    Ignore: yes
    Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

--- debian.riscv-5.15/rules.d/hooks.mk
+++ debian.riscv-5.15/rules.d/hooks.mk
@@ -3,3 +3,9 @@ do_libc_dev_package	= false
 do_doc_package		= false
 do_tools_common		= false
 do_tools_host		= false
+
+# Set inconsistent toolchain
+export GCC_SUFFIX=-10
+# Target
+export CC=$(CROSS_COMPILE)gcc$(GCC_SUFFIX)
+kmake += CC=$(CC)
