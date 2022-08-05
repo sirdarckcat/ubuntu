a1ceedf343ae240efda34b0bde52e19cfdda4575
Enzo Matsumiya <ematsumiya@suse.de>
2022-08-05 11:47:38 -0300

    cifs: remove useless DeleteMidQEntry()
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    DeleteMidQEntry() was just a proxy for cifs_mid_q_entry_release().
    
    - remove DeleteMidQEntry()
    - rename cifs_mid_q_entry_release() to release_mid()
    - rename kref_put() callback _cifs_mid_q_entry_release to __release_mid
    - rename AllocMidQEntry() to alloc_mid()
    - rename cifs_delete_mid() to delete_mid()
    
    Update callers to use new names.
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    (cherry picked from commit 70f08f914a37a0881a7f17068e03ff5a5024822d)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/smb2pdu.c
+++ fs/cifs/smb2pdu.c
@@ -3785,7 +3785,7 @@ smb2_echo_callback(struct mid_q_entry *mid)
 		credits.instance = server->reconnect_instance;
 	}
 
-	DeleteMidQEntry(mid);
+	release_mid(mid);
 	add_credits(server, &credits, CIFS_ECHO_OP);
 }
 
@@ -4210,7 +4210,7 @@ smb2_readv_callback(struct mid_q_entry *mid)
 				     rdata->offset, rdata->got_bytes);
 
 	queue_work(cifsiod_wq, &rdata->work);
-	DeleteMidQEntry(mid);
+	release_mid(mid);
 	add_credits(server, &credits, 0);
 }
 
@@ -4449,7 +4449,7 @@ smb2_writev_callback(struct mid_q_entry *mid)
 				      wdata->offset, wdata->bytes);
 
 	queue_work(cifsiod_wq, &wdata->work);
-	DeleteMidQEntry(mid);
+	release_mid(mid);
 	add_credits(server, &credits, 0);
 }
 
