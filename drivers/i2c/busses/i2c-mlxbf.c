33ffbaef8108f187a4268c25a4386fe2fa99db5c
Asmaa Mnebhi <asmaa@nvidia.com>
2022-08-16 13:31:44 -0400

    Revert "UBUNTU: SAUCE: i2c-mlxbf: fix lock mechanism"
    BugLink: https://bugs.launchpad.net/elbridge/+bug/1986694
    
    This reverts commit 1cba36dc1de60fb2027841683a8934d9adb8ae09.
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Signed-off-by: Ike Panhc <ike.pan@canonical.com>

--- drivers/i2c/busses/i2c-mlxbf.c
+++ drivers/i2c/busses/i2c-mlxbf.c
@@ -764,17 +764,13 @@ static int mlx_smbus_start_transaction(struct mlx_i2c_priv *priv,
 	slave     = request->slave & 0x7f;
 	addr      = slave << 1;
 
-	/* Try to acquire the smbus gw lock before any reads of the GW register since
-	 * a read sets the lock.
-	 */
-	if (WARN_ON(!mlx_smbus_master_lock(priv)))
+	/* First of all, check whether the HW is idle */
+	if (WARN_ON(!mlx_smbus_master_wait_for_idle(priv)))
 		return -EBUSY;
 
-	/* Check whether the HW is idle */
-	if (WARN_ON(!mlx_smbus_master_wait_for_idle(priv))) {
-		mlx_smbus_master_unlock(priv);
+	/* Try to acquire the smbus gw lock */
+	if (WARN_ON(!mlx_smbus_master_lock(priv)))
 		return -EBUSY;
-	}
 
 	/* Set first byte */
 	data_desc[data_idx++] = addr;
@@ -798,10 +794,8 @@ static int mlx_smbus_start_transaction(struct mlx_i2c_priv *priv,
 		if (flags & I2C_F_WRITE) {
 			write_en   = 1;
 			write_len += operation->length;
-			if (data_idx + operation->length > MASTER_DATA_DESC_SIZE) {
-				mlx_smbus_master_unlock(priv);
+			if (data_idx + operation->length > MASTER_DATA_DESC_SIZE)
 				return -ENOBUFS;
-			}
 			memcpy(data_desc + data_idx,
 			       operation->buffer, operation->length);
 			data_idx  += operation->length;
