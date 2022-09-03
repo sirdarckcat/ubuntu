f2d8f271b9944da5df856f0ecd3695e0480d600a
Anup Patel <apatel@ventanamicro.com>
2022-09-03 21:43:05 +0530

    UBUNTU: SAUCE: genirq: Add mechanism to multiplex a single HW IPI
    BugLink: https://bugs.launchpad.net/bugs/1975592
    
    All RISC-V platforms have a single HW IPI provided by the INTC local
    interrupt controller. The HW method to trigger INTC IPI can be through
    external irqchip (e.g. RISC-V AIA), through platform specific device
    (e.g. SiFive CLINT timer), or through firmware (e.g. SBI IPI call).
    
    To support multiple IPIs on RISC-V, we add a generic IPI multiplexing
    mechanism which help us create multiple virtual IPIs using a single
    HW IPI. This generic IPI multiplexing is shared among various RISC-V
    irqchip drivers.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    (cherry picked from https://lore.kernel.org/r/20220903161309.32848-4-apatel@ventanamicro.com)
    Signed-off-by: Emil Renner Berthing <emil.renner.berthing@canonical.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -1249,6 +1249,24 @@ int __ipi_send_mask(struct irq_desc *desc, const struct cpumask *dest);
 int ipi_send_single(unsigned int virq, unsigned int cpu);
 int ipi_send_mask(unsigned int virq, const struct cpumask *dest);
 
+/**
+ * struct ipi_mux_ops - IPI multiplex operations
+ *
+ * @ipi_mux_pre_handle:	Optional function called before handling parent IPI
+ * @ipi_mux_post_handle:Optional function called after handling parent IPI
+ * @ipi_mux_send:	Trigger parent IPI on target CPUs
+ */
+struct ipi_mux_ops {
+	void (*ipi_mux_pre_handle)(unsigned int parent_virq, void *data);
+	void (*ipi_mux_post_handle)(unsigned int parent_virq, void *data);
+	void (*ipi_mux_send)(unsigned int parent_virq, void *data,
+			     const struct cpumask *mask);
+};
+
+void ipi_mux_process(void);
+int ipi_mux_create(unsigned int parent_virq, unsigned int nr_ipi,
+		   const struct ipi_mux_ops *ops, void *data);
+
 #ifdef CONFIG_GENERIC_IRQ_MULTI_HANDLER
 /*
  * Registers a generic IRQ handling function as the top-level IRQ handler in
