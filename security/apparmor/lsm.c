ca50660075925737937c47ffaceb141bab1710c8
Casey Schaufler <casey@schaufler-ca.com>
2022-09-20 09:10:04 -0700

    UBUNTU: SAUCE: apparmor4.0.0 [24/94]: Stacking v38: LSM: Infrastructure management of the sock security
    BugLink: https://bugs.launchpad.net/bugs/2028253
    
    Move management of the sock->sk_security blob out
    of the individual security modules and into the security
    infrastructure. Instead of allocating the blobs from within
    the modules the modules tell the infrastructure how much
    space is required, and the space is allocated there.
    
    Acked-by: Paul Moore <paul@paul-moore.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from git://git.launchpad.net/~apparmor-dev/ubuntu-kernel-next noble-dev)
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -1161,28 +1161,13 @@ static int apparmor_userns_create(const struct cred *cred)
 	return error;
 }
 
-static int apparmor_sk_alloc_security(struct sock *sk, int family, gfp_t flags)
-{
-	struct aa_sk_ctx *ctx;
-
-	ctx = kzalloc(sizeof(*ctx), flags);
-	if (!ctx)
-		return -ENOMEM;
-
-	sk->sk_security = ctx;
-
-	return 0;
-}
-
 static void apparmor_sk_free_security(struct sock *sk)
 {
 	struct aa_sk_ctx *ctx = aa_sock(sk);
 
-	sk->sk_security = NULL;
 	aa_put_label(ctx->label);
 	aa_put_label(ctx->peer);
 	path_put(&ctx->path);
-	kfree(ctx);
 }
 
 /**
@@ -1649,6 +1634,7 @@ struct lsm_blob_sizes apparmor_blob_sizes __ro_after_init = {
 	.lbs_ipc = sizeof(struct aa_ipc_sec),
 	.lbs_msg_msg = sizeof(struct aa_msg_sec),
 	.lbs_superblock = sizeof(struct aa_superblock_sec),
+	.lbs_sock = sizeof(struct aa_sk_ctx),
 };
 
 static struct lsm_id apparmor_lsmid __ro_after_init = {
@@ -1703,7 +1689,6 @@ static struct security_hook_list apparmor_hooks[] __ro_after_init = {
 	LSM_HOOK_INIT(getprocattr, apparmor_getprocattr),
 	LSM_HOOK_INIT(setprocattr, apparmor_setprocattr),
 
-	LSM_HOOK_INIT(sk_alloc_security, apparmor_sk_alloc_security),
 	LSM_HOOK_INIT(sk_free_security, apparmor_sk_free_security),
 	LSM_HOOK_INIT(sk_clone_security, apparmor_sk_clone_security),
 
