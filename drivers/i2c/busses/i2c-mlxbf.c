6b21af23ff6d816b50d8ede564b4003d99125012
Asmaa Mnebhi <asmaa@nvidia.com>
2022-10-11 13:58:52 -0400

    Revert "UBUNTU: SAUCE: i2c-mlxbf.c: Add driver version"
    This reverts commit e75da3a761ccbf4c111db693ebe067d33b8fd736.
    BugLink: https://bugs.launchpad.net/bugs/1991551
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>

--- drivers/i2c/busses/i2c-mlxbf.c
+++ drivers/i2c/busses/i2c-mlxbf.c
@@ -15,8 +15,6 @@
 #include <linux/platform_device.h>
 #include <linux/string.h>
 
-#define DRV_VERSION "3.1"
-
 /* Defines what functionality is present. */
 #define MLXBF_I2C_FUNC_SMBUS_BLOCK \
 	(I2C_FUNC_SMBUS_BLOCK_DATA | I2C_FUNC_SMBUS_BLOCK_PROC_CALL)
@@ -2529,4 +2527,3 @@ MODULE_DESCRIPTION("Mellanox BlueField I2C bus driver");
 MODULE_AUTHOR("Khalil Blaiech <kblaiech@nvidia.com>");
 MODULE_AUTHOR("Asmaa Mnebhi <asmaa@nvidia.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION(DRV_VERSION);
