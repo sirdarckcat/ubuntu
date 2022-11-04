48cca2d27c1bc38394bde6e853b6789e7e6c2c8b
Jakub Kicinski <kuba@kernel.org>
2022-11-04 12:13:33 -0700

    genetlink: introduce split op representation
    BugLink: https://bugs.launchpad.net/bugs/2053155
    
    We currently have two forms of operations - small ops and "full" ops
    (or just ops). The former does not have pointers for some of the less
    commonly used features (namely dump start/done and policy).
    
    The "full" ops, however, still don't contain all the necessary
    information. In particular the policy is per command ID, while
    do and dump often accept different attributes. It's also not
    possible to define different pre_doit and post_doit callbacks
    for different commands within the family.
    
    At the same time a lot of commands do not support dumping and
    therefore all the dump-related information is wasted space.
    
    Create a new command representation which can hold info about
    a do implementation or a dump implementation, but not both at
    the same time.
    
    Use this new representation on the command execution path
    (genl_family_rcv_msg) as we either run a do or a dump and
    don't have to create a "full" op there.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 20b0b53aca436af9fece9428ca3ab7c7b9cf4583)
    Signed-off-by: Tony Duan <yifeid@nvidia.com>
    Signed-off-by: William Tu <witu@nvidia.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    [bzolnier: fix conflict on genl_ops rename in include/net/genetlink.h]
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>

--- net/core/drop_monitor.c
+++ net/core/drop_monitor.c
@@ -1636,7 +1636,7 @@ static const struct genl_small_ops dropmon_ops[] = {
 	},
 };
 
-static int net_dm_nl_pre_doit(const struct genl_ops *ops,
+static int net_dm_nl_pre_doit(const struct genl_split_ops *ops,
 			      struct sk_buff *skb, struct genl_info *info)
 {
 	mutex_lock(&net_dm_mutex);
@@ -1644,7 +1644,7 @@ static int net_dm_nl_pre_doit(const struct genl_ops *ops,
 	return 0;
 }
 
-static void net_dm_nl_post_doit(const struct genl_ops *ops,
+static void net_dm_nl_post_doit(const struct genl_split_ops *ops,
 				struct sk_buff *skb, struct genl_info *info)
 {
 	mutex_unlock(&net_dm_mutex);
