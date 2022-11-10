5690ce008ca57ee196e866a2c73136e37ce8855e
Wen-chien Jesse Sung <jesse.sung@canonical.com>
2022-11-10 22:44:01 +0800

    UBUNTU: [Packaging] Enable arm64 build
    Ignore: yes
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

---
+++ debian.iot/rules.d/arm64.mk
@@ -0,0 +1,27 @@
+human_arch	= ARMv8
+build_arch	= arm64
+header_arch	= arm64
+defconfig	= defconfig
+flavours	= iot
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinuz
+no_dumpfile	= true
+uefi_signed     = false
+loader		= grub
+vdso		= vdso_install
+disable_d_i 	= true
+do_cloud_tools	= false
+do_doc_package	= false
+do_extras_package = false
+do_libc_dev_package = false
+do_source_package = false
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_bpftool = true
+
+do_dtbs	= true
+do_zfs		= true
+do_dkms_wireguard = true
