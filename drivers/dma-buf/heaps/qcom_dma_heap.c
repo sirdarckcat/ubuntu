ad85653ddd46578fb1f7ca51bd3e21370b8d3551
Sayali Patil <quic_sayapati@quicinc.com>
2023-01-12 20:31:36 +0530

    dma-heap: qcom: Add a snapshot of the dma-buf heaps drivers
    Add a snapshot of the dma-buf heaps drivers as of msm-5.15
    commit <a86cb295c99b> ("Merge "mmc: sdhci-msm:
    Dump sdhci registers when dumping cqe registers"") and
    removed android specific trace dependency.
    
    Change-Id: Iaa8518543f122bed957751dc46e46b803fda8357
    Signed-off-by: Sayali Patil <quic_sayapati@quicinc.com>

--- drivers/dma-buf/heaps/qcom_dma_heap.c
+++ drivers/dma-buf/heaps/qcom_dma_heap.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -9,11 +10,13 @@
 #include <linux/err.h>
 
 #include <linux/qcom_dma_heap.h>
+#include <linux/qcom_tui_heap.h>
 #include "qcom_cma_heap.h"
 #include "qcom_dt_parser.h"
 #include "qcom_system_heap.h"
 #include "qcom_carveout_heap.h"
 #include "qcom_secure_system_heap.h"
+#include "qcom_bitstream_contig_heap.h"
 
 static int qcom_dma_heap_probe(struct platform_device *pdev)
 {
@@ -40,41 +43,81 @@ static int qcom_dma_heap_probe(struct platform_device *pdev)
 		switch (heap_data->type) {
 		case HEAP_TYPE_SECURE_CARVEOUT:
 			ret = qcom_secure_carveout_heap_create(heap_data);
-			if (ret < 0)
-				pr_err("%s: DMA-BUF Heap: Failed to create %s, error is %d\n",
-				       __func__, heap_data->name, ret);
-			else if (!ret)
-				pr_info("%s: DMA-BUF Heap: Created %s\n", __func__,
-					heap_data->name);
 			break;
 		case HEAP_TYPE_CARVEOUT:
 			ret = qcom_carveout_heap_create(heap_data);
-			if (ret < 0)
-				pr_err("%s: DMA-BUF Heap: Failed to create %s, error is %d\n",
-				       __func__, heap_data->name, ret);
-			else if (!ret)
-				pr_info("%s: DMA-BUF Heap: Created %s\n", __func__,
-					heap_data->name);
 			break;
 		case HEAP_TYPE_CMA:
 			ret = qcom_add_cma_heap(heap_data);
-			if (ret < 0)
-				pr_err("%s: DMA-BUF Heap: Failed to create %s, error is %d\n",
-				       __func__, heap_data->name, ret);
-			else if (!ret)
-				pr_info("%s: DMA-BUF Heap: Created %s\n", __func__,
-					heap_data->name);
+			break;
+		case HEAP_TYPE_TUI_CARVEOUT:
+			ret = qcom_tui_carveout_heap_create(heap_data);
 			break;
 		default:
 			pr_err("%s: Unknown heap type %u\n", __func__, heap_data->type);
 			break;
 		}
+
+		if (ret)
+			pr_err("%s: DMA-BUF Heap: Failed to create %s, error is %d\n",
+			       __func__, heap_data->name, ret);
+		else
+			pr_info("%s: DMA-BUF Heap: Created %s\n", __func__,
+				heap_data->name);
 	}
+
+	qcom_add_bitstream_contig_heap("system-secure");
 
 	free_pdata(heaps);
 
 	return ret;
 }
+
+static int qcom_dma_heaps_freeze(struct device *dev)
+{
+	int ret;
+
+	ret = qcom_secure_carveout_heap_freeze();
+	if (ret) {
+		pr_err("Failed to freeze secure carveout heap: %d\n", ret);
+		return ret;
+	}
+
+	ret = qcom_secure_system_heap_freeze();
+	if (ret) {
+		pr_err("Failed to freeze secure system heap: %d\n", ret);
+		goto err;
+	}
+
+	return 0;
+err:
+	ret = qcom_secure_carveout_heap_restore();
+	if (ret) {
+		pr_err("Failed to restore secure carveout heap: %d\n", ret);
+		return ret;
+	}
+	return -EBUSY;
+}
+
+static int qcom_dma_heaps_restore(struct device *dev)
+{
+	int ret;
+
+	ret = qcom_secure_carveout_heap_restore();
+	if (ret)
+		pr_err("Failed to restore secure carveout heap: %d\n", ret);
+
+	ret = qcom_secure_system_heap_restore();
+	if (ret)
+		pr_err("Failed to restore secure system heap: %d\n", ret);
+
+	return ret;
+}
+
+static const struct dev_pm_ops qcom_dma_heaps_pm_ops = {
+	.freeze_late = qcom_dma_heaps_freeze,
+	.restore_early = qcom_dma_heaps_restore,
+};
 
 static const struct of_device_id qcom_dma_heap_match_table[] = {
 	{.compatible = "qcom,dma-heaps"},
@@ -86,6 +129,7 @@ static struct platform_driver qcom_dma_heap_driver = {
 	.driver = {
 		.name = "qcom-dma-heap",
 		.of_match_table = qcom_dma_heap_match_table,
+		.pm = &qcom_dma_heaps_pm_ops,
 	},
 };
 
