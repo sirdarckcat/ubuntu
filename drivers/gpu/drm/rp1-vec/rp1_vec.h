e202c1156b988f612c66a103956f660a5ad998b8
Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
2023-02-14 15:04:43 +0000

    drm: Add RP1 VEC driver
    Add support for the RP1 VEC hardware.
    
    Signed-off-by: Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
    (ported from commit 19944891fd295e4b10bb3a45fce7478d3d67e8c0)
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

---
+++ drivers/gpu/drm/rp1-vec/rp1_vec.h
@@ -0,0 +1,80 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * DRM Driver for DSI output on Raspberry Pi RP1
+ */
+
+#include <linux/types.h>
+#include <linux/io.h>
+#include <linux/clk.h>
+#include <drm/drm_device.h>
+#include <drm/drm_simple_kms_helper.h>
+
+#define MODULE_NAME "drm-rp1-vec"
+#define DRIVER_NAME "drm-rp1-vec"
+
+/* ---------------------------------------------------------------------- */
+
+#define RP1VEC_HW_BLOCK_VEC   0
+#define RP1VEC_HW_BLOCK_CFG   1
+#define RP1VEC_NUM_HW_BLOCKS  2
+
+enum {
+	RP1VEC_TVSTD_NTSC = 0,	/* +525 => NTSC       625 => PAL   */
+	RP1VEC_TVSTD_NTSC_J,	/* +525 => NTSC-J     625 => PAL   */
+	RP1VEC_TVSTD_NTSC_443,	/* +525 => NTSC-443  +625 => PAL   */
+	RP1VEC_TVSTD_PAL,	/*  525 => NTSC      +625 => PAL   */
+	RP1VEC_TVSTD_PAL_M,	/* +525 => PAL-M      625 => PAL   */
+	RP1VEC_TVSTD_PAL_N,	/*  525 => NTSC      +625 => PAL-N */
+	RP1VEC_TVSTD_PAL60,	/* +525 => PAL60     +625 => PAL   */
+	RP1VEC_TVSTD_DEFAULT,	/* +525 => NTSC      +625 => PAL   */
+};
+
+/* Which standards support which modes? Those marked with + above */
+#define RP1VEC_TVSTD_SUPPORT_525(n) ((0xD7>>(n)) & 1)
+#define RP1VEC_TVSTD_SUPPORT_625(n) ((0xEC>>(n)) & 1)
+
+/* ---------------------------------------------------------------------- */
+
+struct rp1vec_priv {
+
+	/* DRM and platform device pointers */
+	struct drm_device *drm;
+	struct platform_device *pdev;
+
+	/* Framework and helper objects */
+	struct drm_simple_display_pipe pipe;
+	struct drm_connector connector;
+
+	/* Clock. We assume this is always at 108 MHz. */
+	struct clk *vec_clock;
+
+	/* Block (VCC, CFG) base addresses, and current state */
+	void __iomem *hw_base[RP1VEC_NUM_HW_BLOCKS];
+	u32 cur_fmt;
+	int tv_norm;
+	bool running_on_fpga;
+	bool vec_running, pipe_enabled;
+	struct semaphore finished;
+};
+
+extern const char * const rp1vec_tvstd_names[];
+
+/* ---------------------------------------------------------------------- */
+/* Functions to control the VEC/DMA block				  */
+
+void rp1vec_hw_setup(struct rp1vec_priv *priv,
+		u32 in_format,
+		struct drm_display_mode const *mode,
+		int tvstd);
+void rp1vec_hw_update(struct rp1vec_priv *priv, dma_addr_t addr, u32 offset, u32 stride);
+void rp1vec_hw_stop(struct rp1vec_priv *priv);
+int rp1vec_hw_busy(struct rp1vec_priv *priv);
+irqreturn_t rp1vec_hw_isr(int irq, void *dev);
+void rp1vec_hw_vblank_ctrl(struct rp1vec_priv *priv, int enable);
+
+/* ---------------------------------------------------------------------- */
+/* Functions to control the VIDEO OUT CFG block and check RP1 platform	  */
+
+int  rp1vec_check_platform(struct rp1vec_priv *priv);
+void rp1vec_vidout_setup(struct rp1vec_priv *priv);
+void rp1vec_vidout_poweroff(struct rp1vec_priv *priv);
