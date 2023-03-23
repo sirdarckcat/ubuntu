2ea8dfd3936ca2aa10be51830f5c1a5bc1d195cb
Imre Deak <imre.deak@intel.com>
2023-03-23 16:20:11 +0200

    drm/i915/tc: Move TC port fields to a new intel_tc_port struct
    BugLink: https://bugs.launchpad.net/bugs/2021572
    
    Move the TC port specific fields from intel_digital_port to a new
    intel_tc_port struct. Pass an intel_tc_port pointer to all static
    functions in intel_tc.c keeping dig_port accessible for these via a
    pointer stored in the new struct.
    
    The next patch will allocate the intel_tc_port dynamically, allowing
    moving the struct definition to intel_tc.c.
    
    Reviewed-by: Mika Kahola <mika.kahola@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230323142035.1432621-6-imre.deak@intel.com
    (backported from commit 3eafcddf766b6bb8e6308ec2d587d4ef7a4381e4 linux-next)
    [khfeng: Resolve textual dependency]
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/gpu/drm/i915/display/intel_display.h
+++ drivers/gpu/drm/i915/display/intel_display.h
@@ -272,13 +272,6 @@ enum tc_port {
 	I915_MAX_TC_PORTS
 };
 
-enum tc_port_mode {
-	TC_PORT_DISCONNECTED,
-	TC_PORT_TBT_ALT,
-	TC_PORT_DP_ALT,
-	TC_PORT_LEGACY,
-};
-
 enum dpio_channel {
 	DPIO_CH0,
 	DPIO_CH1
