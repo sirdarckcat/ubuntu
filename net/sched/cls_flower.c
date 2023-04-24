776c69601fffa14137c0a0fc879a9e0d8ca6173e
William Tu <witu@nvidia.com>
2023-04-25 02:51:03 +0300

    Revert "net/sched: flower: fix fl_change() error recovery path"
    This reverts commit 635ef758926c0acdb84cb96cd3b36554b3e7558d.
    
    BugLink: https://bugs.launchpad.net/bugs/2012571
    
    Signed-off-by: William Tu <witu@nvidia.com>
    Acked-by: Andrei Gherzan <andrei.gherzan@canonical.com>
    Acked-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>

--- net/sched/cls_flower.c
+++ net/sched/cls_flower.c
@@ -1844,9 +1844,8 @@ static int fl_change(struct net *net, struct sk_buff *in_skb,
 		fnew->flags = nla_get_u32(tb[TCA_FLOWER_FLAGS]);
 
 		if (!tc_flags_valid(fnew->flags)) {
-			kfree(fnew);
 			err = -EINVAL;
-			goto errout_tb;
+			goto errout;
 		}
 	}
 
@@ -1871,10 +1870,8 @@ static int fl_change(struct net *net, struct sk_buff *in_skb,
 		}
 		spin_unlock(&tp->lock);
 
-		if (err) {
-			kfree(fnew);
-			goto errout_tb;
-		}
+		if (err)
+			goto errout;
 	}
 	fnew->handle = handle;
 
@@ -1983,6 +1980,7 @@ errout_mask:
 	fl_mask_put(head, fnew->mask);
 errout_idr:
 	idr_remove(&head->handle_idr, fnew->handle);
+errout:
 	__fl_put(fnew);
 errout_tb:
 	kfree(tb);
