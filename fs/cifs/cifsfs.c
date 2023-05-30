1544c14a3e4ca11b60552fc2e85c2613de1f8a37
Steve French <stfrench@microsoft.com>
2023-05-30 04:19:20 -0500

    cifs: remove "cifs_" prefix from init/destroy mids functions
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    Rename generic mid functions to same style, i.e. without "cifs_"
    prefix.
    
    cifs_{init,destroy}_mids() -> {init,destroy}_mids()
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -1557,8 +1557,7 @@ cifs_destroy_request_bufs(void)
 	kmem_cache_destroy(cifs_sm_req_cachep);
 }
 
-static int
-cifs_init_mids(void)
+static int init_mids(void)
 {
 	cifs_mid_cachep = kmem_cache_create("cifs_mpx_ids",
 					    sizeof(struct mid_q_entry), 0,
@@ -1576,8 +1575,7 @@ cifs_init_mids(void)
 	return 0;
 }
 
-static void
-cifs_destroy_mids(void)
+static void destroy_mids(void)
 {
 	mempool_destroy(cifs_mid_poolp);
 	kmem_cache_destroy(cifs_mid_cachep);
@@ -1678,7 +1676,7 @@ init_cifs(void)
 	if (rc)
 		goto out_unreg_fscache;
 
-	rc = cifs_init_mids();
+	rc = init_mids();
 	if (rc)
 		goto out_destroy_inodecache;
 
@@ -1735,7 +1733,7 @@ out_destroy_request_bufs:
 #endif
 	cifs_destroy_request_bufs();
 out_destroy_mids:
-	cifs_destroy_mids();
+	destroy_mids();
 out_destroy_inodecache:
 	cifs_destroy_inodecache();
 out_unreg_fscache:
@@ -1773,7 +1771,7 @@ exit_cifs(void)
 	dfs_cache_destroy();
 #endif
 	cifs_destroy_request_bufs();
-	cifs_destroy_mids();
+	destroy_mids();
 	cifs_destroy_inodecache();
 	cifs_fscache_unregister();
 	destroy_workqueue(deferredclose_wq);
