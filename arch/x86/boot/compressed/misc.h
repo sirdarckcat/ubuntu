svsm-vtpm/linux

x86/compressed/acpi: Move EFI system table lookup to helper
Future patches for SEV-SNP-validated CPUID will also require early
parsing of the EFI configuration. Incrementally move the related
code into a set of helpers that can be re-used for that purpose.

Signed-off-by: Michael Roth <michael.roth@amd.com>
Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

arch/x86/boot/compressed/misc.h
@@ -185,10 +185,16 @@ enum efi_type {
 #ifdef CONFIG_EFI
 /* helpers for early EFI config table access */
 enum efi_type efi_get_type(struct boot_params *boot_params);
+unsigned long efi_get_system_table(struct boot_params *boot_params);
 #else
 static inline enum efi_type efi_get_type(struct boot_params *boot_params)
 {
 	return EFI_TYPE_NONE;
+}
+
+static inline unsigned long efi_get_system_table(struct boot_params *boot_params)
+{
+	return 0;
 }
 #endif /* CONFIG_EFI */
 

sirdarckcat/ubuntu

BugLink: http://bugs.launchpad.net/bugs/2001605
(cherry picked from commit 58f3e6b71f42f99ab5d0ab26ddf6e7ee5631f5db)
Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

arch/x86/boot/compressed/misc.h
@@ -181,10 +181,16 @@ enum efi_type {
 #ifdef CONFIG_EFI
 /* helpers for early EFI config table access */
 enum efi_type efi_get_type(struct boot_params *bp);
+unsigned long efi_get_system_table(struct boot_params *bp);
 #else
 static inline enum efi_type efi_get_type(struct boot_params *bp)
 {
 	return EFI_TYPE_NONE;
+}
+
+static inline unsigned long efi_get_system_table(struct boot_params *bp)
+{
+	return 0;
 }
 #endif /* CONFIG_EFI */
 