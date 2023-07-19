f4ade4e6e8b3936b8fe5be279303a7d0c8c053a5
Steve French <stfrench@microsoft.com>
2023-07-18 21:19:23 -0500

    smb: client: fix warning in cifs_smb3_do_mount()
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This fixes the following warning reported by kernel test robot
    
      fs/smb/client/cifsfs.c:982 cifs_smb3_do_mount() warn: possible
      memory leak of 'cifs_sb'
    
    Link: https://lore.kernel.org/all/202306170124.CtQqzf0I-lkp@intel.com/
    Signed-off-by: Paulo Alcantara (SUSE) <pc@manguebit.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    (cherry picked from commit 12c30f33cc6769bf411088a2872843c4f9ea32f9)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -875,9 +875,6 @@ struct dentry *
 cifs_smb3_do_mount(struct file_system_type *fs_type,
 	      int flags, struct smb3_fs_context *old_ctx)
 {
-	int rc;
-	struct super_block *sb = NULL;
-	struct cifs_sb_info *cifs_sb = NULL;
 	struct cifs_mnt_data mnt_data;
 	struct cifs_sb_info *cifs_sb;
 	struct super_block *sb;
@@ -962,13 +959,9 @@ out_super:
 	deactivate_locked_super(sb);
 	return root;
 out:
-	if (cifs_sb) {
-		if (!sb || IS_ERR(sb)) {  /* otherwise kill_sb will handle */
-			kfree(cifs_sb->prepath);
-			smb3_cleanup_fs_context(cifs_sb->ctx);
-			kfree(cifs_sb);
-		}
-	}
+	kfree(cifs_sb->prepath);
+	smb3_cleanup_fs_context(cifs_sb->ctx);
+	kfree(cifs_sb);
 	return root;
 }
 
