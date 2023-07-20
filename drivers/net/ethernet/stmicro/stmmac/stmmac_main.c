fa82675fd60a89cf75e01617c58ece8d37d53b51
Jakub Kicinski <kuba@kernel.org>
2023-07-19 18:04:07 -0700

    eth: stmmac: let page recycling happen with skbs
    BugLink: https://bugs.launchpad.net/bugs/2039720
    
    stmmac removes pages from the page pool after attaching them
    to skbs. Use page recycling instead.
    
    skb heads are always copied, and pages are always from page
    pool in this driver. We could as well mark all allocated skbs
    for recycling.
    
    Reviewed-by: Yunsheng Lin <linyunsheng@huawei.com>
    Link: https://lore.kernel.org/r/20230720010409.1967072-3-kuba@kernel.org
    Reviewed-by: Alexander Lobakin <aleksander.lobakin@intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 98e2727c79d007d99a026eb66f481908e66af263)
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -5418,7 +5418,7 @@ read_again:
 					priv->dma_conf.dma_buf_sz);
 
 			/* Data payload appended into SKB */
-			page_pool_release_page(rx_q->page_pool, buf->page);
+			skb_mark_for_recycle(skb);
 			buf->page = NULL;
 		}
 
@@ -5430,7 +5430,7 @@ read_again:
 					priv->dma_conf.dma_buf_sz);
 
 			/* Data payload appended into SKB */
-			page_pool_release_page(rx_q->page_pool, buf->sec_page);
+			skb_mark_for_recycle(skb);
 			buf->sec_page = NULL;
 		}
 
