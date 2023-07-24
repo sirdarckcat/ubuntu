125fa2ed2c26345d2211291b00b0d93f4695023a
Emil Renner Berthing <emil.renner.berthing@canonical.com>
2023-07-24 15:50:59 +0200

    UBUNTU: [Packaging] Create linux-starfive derivative
    Signed-off-by: Emil Renner Berthing <emil.renner.berthing@canonical.com>

---
+++ debian.starfive/rules.d/riscv64.mk
@@ -0,0 +1,19 @@
+human_arch	= RISC-V
+build_arch	= riscv
+defconfig	= defconfig
+flavours	= starfive
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinuz
+
+vdso		= vdso_install
+no_dumpfile	= true
+
+do_libc_dev_package	= false
+do_extras_package	= true
+do_tools_usbip		= true
+do_tools_cpupower	= true
+do_tools_perf		= true
+do_tools_perf_jvmti	= true
+do_tools_bpftool	= true
+do_dtbs			= true
