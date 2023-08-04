769dfd5d4dcc8222033f938d2167d41256b25566
Kuan-Chung Chen <damon.chen@realtek.com>
2023-08-04 13:34:58 +0800

    wifi: rtw89: Introduce Time Averaged SAR (TAS) feature
    BugLink: https://bugs.launchpad.net/bugs/2045622
    
    Time Averaged SAR (TAS) tracks the amount of transmit power over a
    period of time and adjusts the power accordingly. Two thresholds are
    used to determine when to increase or reduce transmit power: Dynamic
    Power Reduction (DPR) on/off. Compared to Static SAR, which has a
    constant transmit power, TAS can improve the user experience or
    range extension.
    
    TAS can be enabled through BIOS, and the driver will evaluate
    Realtek ACPI DSM with RTW89_ACPI_DSM_FUNC_TAS_EN to determine
    whether TAS should be enabled.
    
    Signed-off-by: Kuan-Chung Chen <damon.chen@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230804053458.31492-1-pkshih@realtek.com
    (backported from commit eb2624f55ad14634f97efbf56cb0cb815800508a)
    [AceLan: conflict with cee427983227a ("wifi: rtw89: phy: refine helpers used for raw TX power") which should be applied after this patch]
    Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
    Acked-by: Anthony Wong <anthony.wong@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/net/wireless/realtek/rtw89/phy.c
+++ drivers/net/wireless/realtek/rtw89/phy.c
@@ -1634,6 +1634,8 @@ s8 rtw89_phy_read_txpwr_limit(struct rtw89_dev *rtwdev, u8 band,
 	const struct rtw89_txpwr_rule_5ghz *rule_5ghz = &rfe_parms->rule_5ghz;
 	const struct rtw89_txpwr_rule_6ghz *rule_6ghz = &rfe_parms->rule_6ghz;
 	struct rtw89_regulatory_info *regulatory = &rtwdev->regulatory;
+	enum nl80211_band nl_band = rtw89_hw_to_nl80211_band(band);
+	u32 freq = ieee80211_channel_to_frequency(ch, nl_band);
 	u8 ch_idx = rtw89_channel_to_idx(rtwdev, band, ch);
 	u8 regd = rtw89_regd_get(rtwdev, band);
 	u8 reg6 = regulatory->reg_6ghz_power;
@@ -1669,7 +1671,7 @@ s8 rtw89_phy_read_txpwr_limit(struct rtw89_dev *rtwdev, u8 band,
 	}
 
 	lmt = rtw89_phy_txpwr_rf_to_mac(rtwdev, lmt);
-	sar = rtw89_query_sar(rtwdev);
+	sar = rtw89_query_sar(rtwdev, freq);
 
 	return min(lmt, sar);
 }
@@ -1889,6 +1891,8 @@ static s8 rtw89_phy_read_txpwr_limit_ru(struct rtw89_dev *rtwdev, u8 band,
 	const struct rtw89_txpwr_rule_5ghz *rule_5ghz = &rfe_parms->rule_5ghz;
 	const struct rtw89_txpwr_rule_6ghz *rule_6ghz = &rfe_parms->rule_6ghz;
 	struct rtw89_regulatory_info *regulatory = &rtwdev->regulatory;
+	enum nl80211_band nl_band = rtw89_hw_to_nl80211_band(band);
+	u32 freq = ieee80211_channel_to_frequency(ch, nl_band);
 	u8 ch_idx = rtw89_channel_to_idx(rtwdev, band, ch);
 	u8 regd = rtw89_regd_get(rtwdev, band);
 	u8 reg6 = regulatory->reg_6ghz_power;
@@ -1924,7 +1928,7 @@ static s8 rtw89_phy_read_txpwr_limit_ru(struct rtw89_dev *rtwdev, u8 band,
 	}
 
 	lmt_ru = rtw89_phy_txpwr_rf_to_mac(rtwdev, lmt_ru);
-	sar = rtw89_query_sar(rtwdev);
+	sar = rtw89_query_sar(rtwdev, freq);
 
 	return min(lmt_ru, sar);
 }
