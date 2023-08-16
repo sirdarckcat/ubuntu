5c120134da71f6f2a4844eaa9f649c45a2b7204d
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2023-08-16 11:16:40 +0800

    UBUNTU: SAUCE: (no-up) drm/i915/display : Remove support for interlace mode
    BugLink: https://bugs.launchpad.net/bugs/2031057
    
    There are multiple panels with which Interlace mode is seen to cause
    flickering (interlacing artifacts) with different platforms.
    
    The issue is seen with both HDMI, DP with CML, CFL, ICL, EHL with
    different display models from Acer, Benq, Dell etc.
    There are some models where the issue is not seen with same setup.
    
    Due to this, cases where multiple display are configured in clone mode,
    sometimes interlaced mode gets picked up in one of the displays
    and flickers are seen.
    
    Since its difficult to pin point if issue is due to the HW or due to
    panels, removing interlace mode for DP and HDMI till this is
    root caused.
    
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/8395
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230614040749.1702127-1-ankit.k.nautiyal@intel.com
    Signed-off-by: Jian Hui Lee <jianhui.lee@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Philip Cox <philip.cox@canonical.com>
    Signed-off-by: Jian Hui Lee <jianhui.lee@canonical.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -4850,8 +4850,6 @@ intel_dp_init_connector(struct intel_digital_port *dig_port,
 	drm_connector_init(dev, connector, &intel_dp_connector_funcs, type);
 	drm_connector_helper_add(connector, &intel_dp_connector_helper_funcs);
 
-	if (!HAS_GMCH(dev_priv) && DISPLAY_VER(dev_priv) < 12)
-		connector->interlace_allowed = true;
 	connector->doublescan_allowed = 0;
 
 	intel_connector->polled = DRM_CONNECTOR_POLL_HPD;
