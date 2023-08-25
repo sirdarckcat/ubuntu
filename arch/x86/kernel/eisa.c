e5e6acbcaf5695901204886c3d80b92538bd2636
Dexuan Cui <decui@microsoft.com>
2023-08-25 01:00:44 +0000

    UBUNTU: SAUCE: x86/EISA: Don't probe EISA bus for a TDX VM (with the paravisor) on Hyper-V
    BugLink: https://bugs.launchpad.net/bugs/2052519
    
    Hyper-V hasn't emulated this MMIO access yet for a TDX VM with the
    pavavisor: in such a VM, the "readl(p)" causes a soft lockup.
    
    Don't probe EISA bus for such a VM for now.
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Acked-by: Marcelo Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- arch/x86/kernel/eisa.c
+++ arch/x86/kernel/eisa.c
@@ -9,11 +9,21 @@
 
 #include <xen/xen.h>
 
+extern bool hyperv_paravisor_present;
+
 static __init int eisa_bus_probe(void)
 {
 	void __iomem *p;
 
 	if ((xen_pv_domain() && !xen_initial_domain()) || cc_platform_has(CC_ATTR_GUEST_SEV_SNP))
+		return 0;
+
+	/*
+	 * Hyper-V hasn't emulated this MMIO access yet for a TDX VM with
+	 * the pavavisor: in such a VM, the "readl(p)" below causes a
+	 * soft lockup. Work around the issue for now.
+	 */
+	if (hyperv_paravisor_present)
 		return 0;
 
 	p = ioremap(0x0FFFD9, 4);
