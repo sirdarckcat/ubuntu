1dad9afc6d80f5babfcf90a4bfcaff7b10ceae40
Tony Duan <yifeid@nvidia.com>
2023-09-06 09:11:23 -0500

    Revert "UBUNTU: SAUCE: xfrm: Check if_id in xfrm full offload"
    BugLink: https://bugs.launchpad.net/bugs/2034578
    
    This reverts commit a3c5055c42629209b7b8b5c3a0e24e77028cd331.
    Linux upstream has full offload solution. To avoid conflict, revert
    this specific commit first.
    
    Signed-off-by: Tony Duan <yifeid@nvidia.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -3511,7 +3511,7 @@ static inline int secpath_has_nontransport(const struct sec_path *sp, int k, int
 }
 
 /* 1 okay. 0 not okay */
-static int check_offload_state_ok(struct net *net, const struct xfrm_tmpl *tmpl, unsigned short family, u32 if_id)
+static int check_offload_state_ok(struct net *net, const struct xfrm_tmpl *tmpl, unsigned short family)
 {
 	struct xfrm_state_offload *xso;
 	struct xfrm_state *x;
@@ -3529,7 +3529,7 @@ static int check_offload_state_ok(struct net *net, const struct xfrm_tmpl *tmpl,
 			if (!(x->xso.flags & XFRM_OFFLOAD_FULL))
 				continue;
 
-			if (xfrm_state_ok(tmpl, x, family, if_id)) {
+			if (xfrm_state_ok(tmpl, x, family)) {
 				found = 1;
 				goto out;
 			}
@@ -3693,7 +3693,7 @@ int __xfrm_policy_check(struct sock *sk, int dir, struct sk_buff *skb,
 			k = xfrm_policy_ok(tpp[i], sp, k, family, if_id);
 			if (k < 0) {
 				if (k == -1) {
-					if (check_offload_state_ok(net, tpp[i], family, if_id) == 1)
+					if (check_offload_state_ok(net, tpp[i], family) == 1)
 						return 1;
 				}
 				if (k < -1)
