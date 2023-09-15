sirdarckcat/ubuntu

commit fbf84fb368923ceedcae4d0782dc4f2d08ff64cf

UBUNTU: SAUCE: platform/x86: dell-uart-backlight: replace chars_in_buffer() with flush_chars()
BugLink: https://bugs.launchpad.net/bugs/2032174

After v6.5, calls tty->driver->ops->chars_in_buffer() may lead to a
deadlock and got soft lockup error. Try using tty->driver->ops->flush_chars()
to archive to same scenario.

There is no functional change, even if uart_flush_chars() doesn't do what
we expected, the driver should be able to keep working, as the backlight
commands is only 3 or 4 bytes short and has little chance to stay in the
queue partially.

Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

drivers/platform/x86/dell/dell-uart-backlight.c
@@ -39,7 +39,8 @@ static struct file *ftty;
 
 unsigned int (*io_serial_in)(struct uart_port *p, int offset);
 int (*uart_write)(struct tty_struct *tty, const unsigned char *buf, int count);
-unsigned int (*uart_chars_in_buffer)(struct tty_struct *tty);
+void (*uart_flush_chars)(struct tty_struct *tty);
+
 
 static bool force;
 module_param(force, bool, 0444);
@@ -160,8 +161,7 @@ static int dell_uart_write(struct uart_8250_port *up, __u8 *buf, int len)
 	tty_port_tty_wakeup(&port->state->port);
 	tty = tty_port_tty_get(&port->state->port);
 	actual = uart_write(tty, buf, len);
-	while (uart_chars_in_buffer(tty))
-		udelay(10);
+	uart_flush_chars(tty);
 
 	return actual;
 }
@@ -401,7 +401,7 @@ static int dell_uart_startup(struct dell_uart_backlight *dell_pdata)
 	tty = port->state->port.tty;
 	io_serial_in = port->serial_in;
 	uart_write = tty->driver->ops->write;
-	uart_chars_in_buffer = tty->driver->ops->chars_in_buffer;
+	uart_flush_chars = tty->driver->ops->flush_chars;
 
 	return 0;
 }

commit 1126a9af648a631e95378c9eeb5c2f135f9654ce

UBUNTU: SAUCE: platform/x86: dell-uart-backlight: add small delay after write command
BugLink: https://bugs.launchpad.net/bugs/2035299

After applying fbf84fb368923 ("UBUNTU: SAUCE: platform/x86:
dell-uart-backlight: replace chars_in_buffer() with flush_chars()"), it
seems that the read() command may fail to receive a response, even when
increasing the retry times. It never gets a response when it fails.

To fix this, try adding a small delay after the write() function as a workaround.

Fixes: fbf84fb368923 ("UBUNTU: SAUCE: platform/x86: dell-uart-backlight: replace chars_in_buffer() with flush_chars()")
Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

drivers/platform/x86/dell/dell-uart-backlight.c
@@ -248,6 +248,7 @@ static int dell_uart_set_bl_power(struct backlight_device *bd, int power)
 	}
 
 	dell_uart_write(uart, bl_cmd->cmd, bl_cmd->tx_len);
+	msleep(1);
 	rx_len = dell_uart_read(uart, bl_cmd->ret, bl_cmd->rx_len);
 
 	mutex_unlock(&dell_pdata->brightness_mutex);
@@ -275,6 +276,7 @@ static int dell_uart_get_brightness(struct backlight_device *bd)
 	}
 
 	dell_uart_write(uart, bl_cmd->cmd, bl_cmd->tx_len);
+	msleep(1);
 	rx_len = dell_uart_read(uart, bl_cmd->ret, bl_cmd->rx_len);
 
 	mutex_unlock(&dell_pdata->brightness_mutex);
@@ -304,6 +306,7 @@ static int dell_uart_update_status(struct backlight_device *bd)
 	}
 
 	dell_uart_write(uart, bl_cmd->cmd, bl_cmd->tx_len);
+	msleep(1);
 	rx_len = dell_uart_read(uart, bl_cmd->ret, bl_cmd->rx_len);
 
 	mutex_unlock(&dell_pdata->brightness_mutex);
@@ -330,6 +333,7 @@ static int dell_uart_show_firmware_ver(struct dell_uart_backlight *dell_pdata)
 	}
 
 	dell_uart_write(uart, bl_cmd->cmd, bl_cmd->tx_len);
+	msleep(1);
 	while (retry-- > 0) {
 		/* first byte is data length */
 		dell_uart_read(uart, bl_cmd->ret, 1);
@@ -371,6 +375,7 @@ static int dell_uart_get_display_mode(struct dell_uart_backlight *dell_pdata)
 		}
 
 		dell_uart_write(uart, bl_cmd->cmd, bl_cmd->tx_len);
+		msleep(1);
 		rx_len = dell_uart_read(uart, bl_cmd->ret, bl_cmd->rx_len);
 
 		mutex_unlock(&dell_pdata->brightness_mutex);