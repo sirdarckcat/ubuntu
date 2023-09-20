362f6c902040d8090eb039850360597c8a58856d
Asmaa Mnebhi <asmaa@nvidia.com>
2023-09-20 13:38:45 -0400

    UBUNTU: SAUCE: mlxbf-gige: Enable the OOB port in mlxbf_gige_open
    BugLink: https://bugs.launchpad.net/bugs/2035128
    
    At the moment, the OOB port is enabled in the mlxbf_gige_probe
    function. If the mlxbf_gige_open is not executed, this could cause
    pause frames to increase in the case where there is high backgroud
    traffic. This results in clogging the BMC port as well.
    Move enabling the OOB port to mlxbf_gige_open.
    
    Signed-off-by: Asmaa Mnebhi <asmaa@nvidia.com>
    Acked-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>
    Acked-by: Agathe Porte <agathe.porte@canonical.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bartlomiej.zolnierkiewicz@canonical.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_main.c
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige_main.c
@@ -138,8 +138,14 @@ static int mlxbf_gige_open(struct net_device *netdev)
 {
 	struct mlxbf_gige *priv = netdev_priv(netdev);
 	struct phy_device *phydev = netdev->phydev;
+	u64 control;
 	u64 int_en;
 	int err;
+
+	/* Perform general init of GigE block */
+	control = readq(priv->base + MLXBF_GIGE_CONTROL);
+	control |= MLXBF_GIGE_CONTROL_PORT_EN;
+	writeq(control, priv->base + MLXBF_GIGE_CONTROL);
 
 	err = mlxbf_gige_request_irqs(priv);
 	if (err)
@@ -291,7 +297,6 @@ static int mlxbf_gige_probe(struct platform_device *pdev)
 	void __iomem *base;
 	int addr, phy_irq;
 	u32 version;
-	u64 control;
 	int err;
 
 	version = 0;
@@ -330,11 +335,6 @@ static int mlxbf_gige_probe(struct platform_device *pdev)
 	plu_base = devm_ioremap_resource(&pdev->dev, plu_res);
 	if (IS_ERR(plu_base))
 		return PTR_ERR(plu_base);
-
-	/* Perform general init of GigE block */
-	control = readq(base + MLXBF_GIGE_CONTROL);
-	control |= MLXBF_GIGE_CONTROL_PORT_EN;
-	writeq(control, base + MLXBF_GIGE_CONTROL);
 
 	netdev = devm_alloc_etherdev(&pdev->dev, sizeof(*priv));
 	if (!netdev)
