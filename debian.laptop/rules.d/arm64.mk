536ab77da0c7e99bc7e1df09dcb018782ca81fe7
Juerg Haefliger <juerg.haefliger@canonical.com>
2023-09-26 19:46:49 +0200

    UBUNTU: [Packaging] laptop: Clean up rules.d/amd64.mk
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

--- debian.laptop/rules.d/arm64.mk
+++ debian.laptop/rules.d/arm64.mk
@@ -1,28 +1,25 @@
-human_arch	= ARMv8
-build_arch	= arm64
-header_arch	= arm64
-defconfig	= defconfig
-flavours	= laptop
-build_image	= Image.gz
-kernel_file	= arch/$(build_arch)/boot/Image.gz
-install_file	= vmlinuz
-no_dumpfile = true
-uefi_signed     = true
+human_arch		= ARMv8
+build_arch		= arm64
+header_arch		= arm64
+defconfig		= defconfig
+flavours		= laptop
+build_image		= Image.gz
+kernel_file		= arch/$(build_arch)/boot/Image.gz
+install_file		= vmlinuz
+no_dumpfile		= true
+uefi_signed		= false
 
-vdso		= vdso_install
+vdso			= vdso_install
 
-do_extras_package = true
-do_tools_usbip  = true
-do_tools_cpupower = true
-do_tools_perf   = true
-do_tools_perf_jvmti = true
-do_tools_bpftool = true
-
-do_dtbs		= true
-
-# laptop
-do_doc_package		= false
 do_extras_package	= false
+do_tools_usbip		= true
+do_tools_cpupower	= true
+do_tools_perf		= true
+do_tools_perf_jvmti	= true
+do_tools_bpftool	= true
+
+do_dtbs			= true
+
 do_libc_dev_package	= false
+do_doc_package		= false
 do_source_package	= false
-do_tools_hyperv		= false
