5057dc470a8be839fc7cdc95c98bcb04f22d7660
Mika Westerberg <mika.westerberg@linux.intel.com>
2023-10-02 10:00:44 +0300

    UBUNTU: SAUCE: PCI/ASPM: Add back L1 PM Substate save and restore
    BugLink: https://bugs.launchpad.net/bugs/2042500
    
    Commit a7152be79b62 ("Revert "PCI/ASPM: Save L1 PM Substates Capability
    for suspend/resume"") reverted saving and restoring of ASPM L1 Substates
    due to a regression that caused resume from suspend to fail on certain
    systems. However, we never added this capability back and this is now
    causing systems fail to enter low power CPU states, drawing more power
    from the battery.
    
    The original revert mentioned that we restore L1 PM substate configuration
    even though ASPM L1 may already be enabled. This is due the fact that
    the pci_restore_aspm_l1ss_state() was called before pci_restore_pcie_state().
    
    Try to enable this functionality again following PCIe r6.0.1, sec 5.5.4
    more closely by:
    
      1) Do not restore ASPM configuration in pci_restore_pcie_state() but
         do that after PCIe capability is restored in pci_restore_aspm_state()
         following PCIe r6.0, sec 5.5.4.
    
      2) ASPM is first enabled on the upstream component and then downstream
         (this is already forced by the parent-child ordering of Linux
         Device Power Management framework).
    
      3) Program ASPM L1 PM substate configuration before L1 enables.
    
      4) Program ASPM L1 PM substate enables last after rest of the fields
         in the capability are programmed.
    
      5) Add denylist that skips restoring on the ASUS and TUXEDO systems
         where these regressions happened, just in case. For the TUXEDO case
         we only skip restore if the BIOS is involved in system suspend
         (that's forcing "mem_sleep=deep" in the command line). This is to
         avoid possible power regression when the default suspend to idle is
         used, and at the same time make sure the devices continue working
         after resume when the BIOS is involved.
    
    Reported-by: Koba Ko <koba.ko@canonical.com>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=217321
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216782
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216877
    Cc: Tasev Nikola <tasev.stefanoska@skynet.be>
    Cc: Mark Enriquez <enriquezmark36@gmail.com>
    Cc: Thomas Witt <kernel@witt.link>
    Cc: Werner Sembach <wse@tuxedocomputers.com>
    Tested-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    (backported from https://lore.kernel.org/all/20231002070044.2299644-1-mika.westerberg@linux.intel.com/)
    Signed-off-by: Koba Ko <koba.ko@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -562,10 +562,14 @@ bool pcie_wait_for_link(struct pci_dev *pdev, bool active);
 void pcie_aspm_init_link_state(struct pci_dev *pdev);
 void pcie_aspm_exit_link_state(struct pci_dev *pdev);
 void pcie_aspm_powersave_config_link(struct pci_dev *pdev);
+void pci_save_aspm_state(struct pci_dev *pdev);
+void pci_restore_aspm_state(struct pci_dev *pdev);
 #else
 static inline void pcie_aspm_init_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_exit_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_powersave_config_link(struct pci_dev *pdev) { }
+static inline void pci_save_aspm_state(struct pci_dev *pdev) { }
+static inline void pci_restore_aspm_state(struct pci_dev *pdev) { }
 #endif
 
 #ifdef CONFIG_PCIE_ECRC
