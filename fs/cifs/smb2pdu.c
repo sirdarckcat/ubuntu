ce5b7b61643f5fcfe22efe16ea673802b07bc8c8
Tim Gardner <tim.gardner@canonical.com>
2023-10-10 09:04:36 -0600

    Revert "smb3: use netname when available on secondary channels"
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This reverts commit 52c57d5e4ffedbfa244414171482109694c693cf.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/smb2pdu.c
+++ fs/cifs/smb2pdu.c
@@ -543,7 +543,6 @@ assemble_neg_contexts(struct smb2_negotiate_req *req,
 		      struct TCP_Server_Info *server, unsigned int *total_len)
 {
 	char *pneg_ctxt;
-	char *hostname = NULL;
 	unsigned int ctxt_len, neg_context_count;
 
 	if (*total_len > 200) {
@@ -575,15 +574,9 @@ assemble_neg_contexts(struct smb2_negotiate_req *req,
 	*total_len += sizeof(struct smb2_posix_neg_context);
 	pneg_ctxt += sizeof(struct smb2_posix_neg_context);
 
-	/*
-	 * secondary channels don't have the hostname field populated
-	 * use the hostname field in the primary channel instead
-	 */
-	hostname = CIFS_SERVER_IS_CHAN(server) ?
-		server->primary_server->hostname : server->hostname;
-	if (hostname && (hostname[0] != 0)) {
+	if (server->hostname && (server->hostname[0] != 0)) {
 		ctxt_len = build_netname_ctxt((struct smb2_netname_neg_context *)pneg_ctxt,
-					      hostname);
+					server->hostname);
 		*total_len += ctxt_len;
 		pneg_ctxt += ctxt_len;
 		neg_context_count = 4;
