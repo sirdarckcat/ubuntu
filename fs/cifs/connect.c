22dbe357b38b638cdb006d85eee2e66f6523e6cb
Tim Gardner <tim.gardner@canonical.com>
2023-10-10 09:04:46 -0600

    Revert "cifs: force new session setup and tcon for dfs"
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This reverts commit 02a9b70d3041867b7aca326d2e1057b599ffd030.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -453,7 +453,9 @@ static int reconnect_target_unlocked(struct TCP_Server_Info *server, struct dfs_
 	return rc;
 }
 
-static int reconnect_dfs_server(struct TCP_Server_Info *server)
+static int
+reconnect_dfs_server(struct TCP_Server_Info *server,
+		     bool mark_smb_session)
 {
 	int rc = 0;
 	const char *refpath = server->current_fullpath + 1;
@@ -477,12 +479,7 @@ static int reconnect_dfs_server(struct TCP_Server_Info *server)
 	if (!cifs_tcp_ses_needs_reconnect(server, num_targets))
 		return 0;
 
-	/*
-	 * Unconditionally mark all sessions & tcons for reconnect as we might be connecting to a
-	 * different server or share during failover.  It could be improved by adding some logic to
-	 * only do that in case it connects to a different server or share, though.
-	 */
-	cifs_mark_tcp_ses_conns_for_reconnect(server, true);
+	cifs_mark_tcp_ses_conns_for_reconnect(server, mark_smb_session);
 
 	cifs_abort_connection(server);
 
@@ -540,7 +537,7 @@ int cifs_reconnect(struct TCP_Server_Info *server, bool mark_smb_session)
 	}
 	spin_unlock(&cifs_tcp_ses_lock);
 
-	return reconnect_dfs_server(server);
+	return reconnect_dfs_server(server, mark_smb_session);
 }
 #else
 int cifs_reconnect(struct TCP_Server_Info *server, bool mark_smb_session)
