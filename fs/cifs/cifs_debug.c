d554ebe36aa03a52d9a553ecd48bfb2949b259ba
Tim Gardner <tim.gardner@canonical.com>
2023-10-10 09:05:05 -0600

    Revert "cifs: connect individual channel servers to primary channel server"
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This reverts commit b84a986e063940bb74c873ca9923bf54e45e2cc8.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/cifs_debug.c
+++ fs/cifs/cifs_debug.c
@@ -272,8 +272,7 @@ static int cifs_debug_data_proc_show(struct seq_file *m, void *v)
 	c = 0;
 	spin_lock(&cifs_tcp_ses_lock);
 	list_for_each_entry(server, &cifs_tcp_ses_list, tcp_ses_list) {
-		/* channel info will be printed as a part of sessions below */
-		if (CIFS_SERVER_IS_CHAN(server))
+		if (server->is_channel)
 			continue;
 
 		c++;
