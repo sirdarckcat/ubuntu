765e7fc9a6da847cfbc5d1a267253c47e9a979d2
Tim Gardner <tim.gardner@canonical.com>
2023-10-10 09:05:07 -0600

    Revert "cifs: release lock earlier in dequeue_mid error case"
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This reverts commit 7d9a40de731131a323c1520e96e14cf449c0c172.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -704,14 +704,13 @@ dequeue_mid(struct mid_q_entry *mid, bool malformed)
 	 * Trying to handle/dequeue a mid after the send_recv()
 	 * function has finished processing it is a bug.
 	 */
-	if (mid->mid_flags & MID_DELETED) {
-		spin_unlock(&GlobalMid_Lock);
+	if (mid->mid_flags & MID_DELETED)
 		pr_warn_once("trying to dequeue a deleted mid\n");
-	} else {
+	else {
 		list_del_init(&mid->qhead);
 		mid->mid_flags |= MID_DELETED;
-		spin_unlock(&GlobalMid_Lock);
 	}
+	spin_unlock(&GlobalMid_Lock);
 }
 
 static unsigned int
