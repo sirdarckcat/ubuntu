706b348847ec748a7e04c74459dec0e6de703ea4
Tim Gardner <tim.gardner@canonical.com>
2023-10-10 09:04:50 -0600

    Revert "cifs: truncate the inode and mapping when we simulate fcollapse"
    BugLink: https://bugs.launchpad.net/bugs/2036450
    
    This reverts commit 8036781cc171a15696859c43438b4fb0a47531d7.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/cifs/smb2ops.c
+++ fs/cifs/smb2ops.c
@@ -25,7 +25,6 @@
 #include "smb2glob.h"
 #include "cifs_ioctl.h"
 #include "smbdirect.h"
-#include "fscache.h"
 #include "fs_context.h"
 
 /* Change credits for different ops and return the total number of credits */
@@ -3914,37 +3913,29 @@ static long smb3_collapse_range(struct file *file, struct cifs_tcon *tcon,
 {
 	int rc;
 	unsigned int xid;
-	struct inode *inode;
 	struct cifsFileInfo *cfile = file->private_data;
-	struct cifsInodeInfo *cifsi;
 	__le64 eof;
 
 	xid = get_xid();
 
-	inode = d_inode(cfile->dentry);
-	cifsi = CIFS_I(inode);
-
-	if (off >= i_size_read(inode) ||
-	    off + len >= i_size_read(inode)) {
+	if (off >= i_size_read(file->f_inode) ||
+	    off + len >= i_size_read(file->f_inode)) {
 		rc = -EINVAL;
 		goto out;
 	}
 
 	rc = smb2_copychunk_range(xid, cfile, cfile, off + len,
-				  i_size_read(inode) - off - len, off);
+				  i_size_read(file->f_inode) - off - len, off);
 	if (rc < 0)
 		goto out;
 
-	eof = cpu_to_le64(i_size_read(inode) - len);
+	eof = cpu_to_le64(i_size_read(file->f_inode) - len);
 	rc = SMB2_set_eof(xid, tcon, cfile->fid.persistent_fid,
 			  cfile->fid.volatile_fid, cfile->pid, &eof);
 	if (rc < 0)
 		goto out;
 
 	rc = 0;
-
-	cifsi->server_eof = i_size_read(inode) - len;
-	truncate_setsize(inode, cifsi->server_eof);
  out:
 	free_xid(xid);
 	return rc;
