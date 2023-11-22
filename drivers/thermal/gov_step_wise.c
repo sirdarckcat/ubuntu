sirdarckcat/ubuntu

commit f6264057efc86fbe123d34f4f24e355a0b79cc94

drivers: thermal: step_wise: avoid throttling at hysteresis temperature after dropping below it
Signed-off-by: Serge Schneider <serge@raspberrypi.org>
(cherry picked from commit 41fdbd71fa0929fd0018aec03392536c624e2537 rpi-6.5.y)
Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>

drivers/thermal/gov_step_wise.c
@@ -98,13 +98,12 @@ static void thermal_zone_trip_update(struct thermal_zone_device *tz, int trip_id
 		trace_thermal_zone_trip(tz, trip_id, trip.type);
 	}
 
-	tz->ops->get_trip_temp(tz, trip_id, &trip_temp);
-	hyst_temp = trip_temp;
+	hyst_temp = trip.temperature;
 	if (tz->ops->get_trip_hyst) {
 		tz->ops->get_trip_hyst(tz, trip_id, &hyst_temp);
 		hyst_temp = trip_temp - hyst_temp;
 	}
-	tz->ops->get_trip_type(tz, trip_id, &trip_type);
+	trip_type = trip.type;
 
 	trend = get_tz_trend(tz, trip_id);
 
@@ -124,9 +123,9 @@ static void thermal_zone_trip_update(struct thermal_zone_device *tz, int trip_id
 		 */
 		if (tz->temperature >= trip_temp ||
 		   (tz->temperature >= hyst_temp &&
-		   old_target != THERMAL_NO_TARGET)) {
+		   old_target == instance->upper)) {
 			throttle = true;
-			trace_thermal_zone_trip(tz, trip, trip_type);
+			trace_thermal_zone_trip(tz, trip_id, trip_type);
 		}
 
 		instance->target = get_target_state(instance, trend, throttle);

commit 7022db866ead0ddee005167ae34c7af5549e1196

UBUNTU: driver: thermal: step_wise: Fix uninitialized variable
BugLink: https://bugs.launchpad.net/bugs/2041741

A bad forward port of a pi-specific commit dropped the initialization
of the trip_temp variable which results in the fan always spinning at
full speed. Fix that.

While at it, drop the second (unnecessary/benign) call of get_tz_trend()
to match rpi-6.5.y.

Fixes: f6264057efc8 ("drivers: thermal: step_wise: avoid throttling at hysteresis temperature after dropping below it")
Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Agathe Porte <agathe.porte@canonical.com>
Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

drivers/thermal/gov_step_wise.c
@@ -98,14 +98,12 @@ static void thermal_zone_trip_update(struct thermal_zone_device *tz, int trip_id
 		trace_thermal_zone_trip(tz, trip_id, trip.type);
 	}
 
-	hyst_temp = trip.temperature;
+	hyst_temp = trip_temp = trip.temperature;
 	if (tz->ops->get_trip_hyst) {
 		tz->ops->get_trip_hyst(tz, trip_id, &hyst_temp);
 		hyst_temp = trip_temp - hyst_temp;
 	}
 	trip_type = trip.type;
-
-	trend = get_tz_trend(tz, trip_id);
 
 	dev_dbg(&tz->device,
 		"Trip%d[type=%d,temp=%d,hyst=%d]:trend=%d,throttle=%d\n",