9aa9699a192ad342bf986db39dcb72b3e6dc13bf
Timo Aaltonen <timo.aaltonen@canonical.com>
2023-12-15 22:28:57 +0200

    Revert "UBUNTU: SAUCE: apparmor: patch to provide compatibility with v2.x net rules"
    BugLink: https://bugs.launchpad.net/bugs/2046577
    
    This reverts commit e4b5d05b2710ce6c1d326a09079a874d24261e07.
    
    Jammy has apparmor v3, no need to support older rules.
    
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- security/apparmor/include/policy.h
+++ security/apparmor/include/policy.h
@@ -112,7 +112,6 @@ struct aa_data {
  * @policy: general match rules governing policy
  * @file: The set of rules governing basic file access and domain transitions
  * @caps: capabilities for the profile
- * @net_compat: v2 compat network controls for the profile
  * @rlimits: rlimits for the profile
  *
  * @dents: dentries for the profiles file entries in apparmorfs
@@ -150,7 +149,6 @@ struct aa_profile {
 	struct aa_policydb policy;
 	struct aa_file_rules file;
 	struct aa_caps caps;
-	struct aa_net_compat *net_compat;
 
 	int xattr_count;
 	char **xattrs;
@@ -235,9 +233,7 @@ static inline unsigned int PROFILE_MEDIATES_AF(struct aa_profile *profile,
 	__be16 be_af = cpu_to_be16(AF);
 
 	if (!state) {
-		state = PROFILE_MEDIATES(profile, AA_CLASS_NET_COMPAT);
-		if (!state)
-			return 0;
+		return 0;
 	}
 	state = aa_dfa_match_len(profile->policy.dfa, state, (char *) &be_af, 2);
 	return state;
