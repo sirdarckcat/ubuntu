fcc4656c3c647a08c4fe3aea6a740e56201cdee9
John Cabaj <john.cabaj@canonical.com>
2024-05-06 21:22:22 -0500

    UBUNTU: Start new release
    Ignore: yes
    Signed-off-by: John Cabaj <john.cabaj@canonical.com>

--- debian.ibm-6.8/rules.d/hooks.mk
+++ debian.ibm-6.8/rules.d/hooks.mk
@@ -1,1 +1,8 @@
-do_enforce_all = true
+do_enforce_all      = true
+do_libc_dev_package = false
+do_doc_package      = false
+do_tools_common     = false
+do_tools_host       = false
+do_lib_rust         = false
+do_odm_drivers      = false
+gcc                 = gcc-12
